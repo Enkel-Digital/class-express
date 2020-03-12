e1d8af2f499f038f1716ea5c239057a6b2de629f
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-13 03:03:14 +0800

    Added src files + Home and About views.

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app id="app">
+    <!-- Router view always on top -->
+    <v-content>
+      <router-view />
+    </v-content>
+
+    <!-- Bottom navigation bar stays the same across the screens -->
+    <v-bottom-navigation app>
+      <v-btn to="/">
+        <span>Home</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn to="/explore">
+        <span>Explore</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn to="/upcoming">
+        <span>Upcoming</span>
+        <v-icon>mdi-calendar-today</v-icon>
+      </v-btn>
+
+      <v-btn to="/profile">
+        <span>Profile</span>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<style>
+/* Basic default styles applied throughout the app */
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
