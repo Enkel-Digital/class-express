677b1ce34b738c0bf510d3f080a0ac1b63b9e9fe
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-14 17:34:27 +0800

    Added firebase to project and added setup code: 1.  Added firebase package. 2.  Updated main.js to setup firebase first on startup.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,28 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+// firebaseConfig auto generated in project settings
+firebase.initializeApp({
+  apiKey: "AIzaSyAXQkdXjxHpZbEYNTXoyfyBXfWVEbm-tCA",
+  authDomain: "classes-ekd.firebaseapp.com",
+  databaseURL: "https://classes-ekd.firebaseio.com",
+  projectId: "classes-ekd",
+  storageBucket: "classes-ekd.appspot.com",
+  messagingSenderId: "385087995070",
+  appId: "1:385087995070:web:7204f5d15cb9004c3072ef"
+});
+
+// Wait for firebase to finish initialization before creating the app.
+// So that the router navigation wont break due to invalid auth
+firebase.auth().onAuthStateChanged(() => {
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
+});
