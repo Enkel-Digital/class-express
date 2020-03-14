a259622ebb41c525e18a0719e236f4a94afdf2d1
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-15 02:31:56 +0800

    Created back button component.

---
+++ app/src/components/BackBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-responsive class="ma-0 pa-0">
+    <v-btn icon @click="goBack">
+      <v-icon>mdi-keyboard-backspace</v-icon>
+    </v-btn>
+  </v-responsive>
+</template>
+
+<script>
+export default {
+  name: "BackBtn",
+  methods: {
+    goBack() {
+      this.$router.back();
+    }
+  }
+};
+</script>
