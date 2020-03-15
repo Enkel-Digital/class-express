47f092bb40b750b0decdb90ef30823a67349c90c
leeszeray <leeszeray@outlook.com>
2020-03-15 18:40:07 +0800

    completed basic upcoming page, however there exists issues with formatting the datetime

--- app/src/views/Upcoming.vue
+++ app/src/views/Upcoming.vue
@@ -2,7 +2,38 @@
   <v-content class="home">
     <v-card v-for="clas in upcomingClasses" :key="clas.className">
       <!-- Log out the data first, to build the UI using vuetify to represent the data. -->
-      {{ JSON.stringify(clas) }}
+      <!-- {{ JSON.stringify(clas) }} -->
+      <v-img
+        class="white--text align-end"
+        height="200px"
+         src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title>{{clas.className}}</v-card-title>
+      </v-img>
+      <v-card-subtitle class="pb-0">{{clas.time}} to {{clas.time + clas.classLength}}</v-card-subtitle>
+      <v-card-text class="text--primary">
+      <div>{{clas.classProvider.name}}</div>
+
+      <div>{{clas.location}}</div>
+      </v-card-text>
+
+      <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Class Details
+      </v-btn>
+      <!-- TODO: LINK SOMEWHERE  -->
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+      <!-- TODO: LINK SOMEWHERE  -->
+
+    </v-card-actions>
     </v-card>
   </v-content>
 </template>
@@ -14,6 +45,7 @@
  */
 
 export default {
+
   name: "upcoming",
   data() {
     return {
@@ -27,7 +59,7 @@ export default {
             name: "Guitar Studio 1", // Name of the provider
             id: 123
           },
-          location: "" // Location Coordinates so we can show on a Map insert
+          location: "Guitar location" // Location Coordinates so we can show on a Map insert
         },
         {
           className: "advanced cooking",
@@ -37,12 +69,13 @@ export default {
             name: "Tampines CC", // Name of the provider
             id: 456
           },
-          location: "" // Location Coordinates so we can show on a Map insert
+          location: "cooking location" // Location Coordinates so we can show on a Map insert
         }
       ]
     };
   }
 };
+
 </script>
 
 <style scoped></style>
