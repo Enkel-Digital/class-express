73024bc8ab8dad418e896ee33102c3804a4245a3
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-19 00:53:52 +0800

    Renamed use of 404 to NotFound and updated view: 1.  Renamed all 404 modules to NotFound modules as JS cannot use num as     value naming. 2.  Updated all use of 404 to NotFound 3.  Updated UI of NotFound and added auto redirect after timeout.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-app-bar color="orange lighten-1" flat dark>
+      <BackBtn />
+      <v-toolbar-title>Page Not Found</v-toolbar-title>
+    </v-app-bar>
+
+    <div id="notFound">
+      <br />
+      <p>Oops we are really sorry but the page is not found...</p>
+      <p>You will be redirected home in 3 seconds...</p>
+
+      <br />
+      Go home now?
+      <v-btn :to="{ name: 'home' }">Home</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import BackBtn from "@/components/BackBtn";
+
+export default {
+  name: "notFound",
+  components: {
+    BackBtn
+  },
+  mounted() {
+    setTimeout(
+      router => {
+        // Redirect back home
+        router.push({ name: "home" });
+      },
+      3000,
+      this.$router
+    );
+  }
+};
+</script>
+
+<style scoped>
+#notFound {
+  margin: 1em;
+  text-align: left;
+}
+</style>
