d334fd94cc1969e71f9708157a5b85590bc06c7d
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-18 22:28:47 +0800

    Updated UI of Settings view.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,64 +1,63 @@
 <template>
-  <v-content class="profile">
+  <v-content class="settings">
     <!-- Add a loader banner showing the update process and notify user if failed. -->
 
     <v-toolbar color="orange lighten-1" flat dark>
       <BackBtn />
-
       <v-toolbar-title>Settings</v-toolbar-title>
 
-      <!-- <v-btn icon> -->
-      <!-- <v-icon>mdi-magnify</v-icon> -->
-      <!-- </v-btn> -->
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-toolbar>
 
-    <v-row justify="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-list>
-          <v-subheader colors="orange">
-            Notifications:
-          </v-subheader>
+    <v-list>
+      <v-subheader colors="orange">
+        Notifications:
+      </v-subheader>
 
-          <div v-ripple @click="mobileNotification = !mobileNotification">
-            <v-checkbox
-              v-model="mobileNotification"
-              readonly
-              :label="'Mobile notifications'"
-              class="ma-1 pa-0"
-            ></v-checkbox>
-          </div>
+      <div v-ripple @click="mobileNotification = !mobileNotification">
+        <v-checkbox
+          v-model="mobileNotification"
+          readonly
+          :label="'Mobile notifications'"
+          class="ma-1 pa-0"
+        ></v-checkbox>
+      </div>
 
-          <div v-ripple @click="emailNotification = !emailNotification">
-            <v-checkbox
-              v-model="emailNotification"
-              readonly
-              :label="'Email notifications'"
-              class="ma-1 pa-0"
-            ></v-checkbox>
-          </div>
-        </v-list>
+      <div v-ripple @click="emailNotification = !emailNotification">
+        <v-checkbox
+          v-model="emailNotification"
+          readonly
+          :label="'Email notifications'"
+          class="ma-1 pa-0"
+        ></v-checkbox>
+      </div>
+    </v-list>
 
-        <v-divider></v-divider>
+    <v-divider></v-divider>
 
-        <v-list>
-          <v-subheader colors="orange">
-            Billing:
-          </v-subheader>
+    <v-list>
+      <v-subheader colors="orange">
+        Billing:
+      </v-subheader>
 
-          <v-list-item ripple>
-            See Billing info
-          </v-list-item>
+      <v-list-item ripple>
+        See Billing info
+      </v-list-item>
 
-          <v-list-item ripple>
-            View recent charges
-          </v-list-item>
-        </v-list>
-      </v-col>
-    </v-row>
+      <v-list-item ripple>
+        View recent charges
+      </v-list-item>
+    </v-list>
 
-    <v-list-item @click="logout" ripple style="background-color: #ededed;">
-      Logout
-    </v-list-item>
+    <v-list>
+      <v-list-item @click="logout" ripple style="background-color: #ededed;">
+        Logout
+      </v-list-item>
+    </v-list>
   </v-content>
 </template>
 
@@ -67,7 +66,7 @@ import logout from "@/controllers/logout";
 import BackBtn from "@/components/BackBtn";
 
 export default {
-  name: "profile",
+  name: "settings",
   components: {
     BackBtn
   },
