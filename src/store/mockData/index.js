28a6317fc36151cd07d5fda49cdf19d7c12e9b62
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-20 22:03:06 +0800

    Mockdata, vuex use, Profile view, initialState: 1.  Created mockdata/ folder and created a module to export some mockdata. 2.  Created new action in vuex store to populate user details, mainly     user and points for now into state from mockdata. 3.  Updated order of user data exported from initialState function. 4.  Added refresh icon in Profile view's app bar to call getUserDetails     action in vuex to import in the mockData, mainly used for testing now. 5.  Updated data interpolation in Profile view's template due to state     structure change.

---
+++ src/store/mockData/index.js
@@ -0,0 +1,29 @@
+/**
+ * Export mock data from here for testing UI before backend is created
+ */
+
+const user = {
+  name: "JJ Lee",
+  email: "JJ@enkeldigital.com",
+  picture: undefined,
+  location: {
+    country: "" // Used for the default search location
+  }
+};
+
+const points = {
+  left: 20,
+  total: 45,
+  pointsPeriod: {
+    timezone: "",
+    start: "",
+    end: "20 / 4 / 2020" // To change to some other datetime format
+  }
+};
+
+const paymentDetails = {
+  userId: "",
+  paymentProvider: "" // Enum of credit card or SaaS like paypal/stripe
+};
+
+module.exports = { user, points, paymentDetails };
