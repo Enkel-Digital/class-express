53d6133967e8cdc1469d837d9139f2a76a6b4227
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-21 19:19:03 +0800

    Updated NotFound UI and redirect logic: 1.  Fixed the app bar UI and wording. 2.  Updated the redirect logic to fix bug where timeout is not cleared.     Fixed it now so that when user navigates away the timeout is cleared     and instead of always redirecting back home, redirect back to the     last used route/view if available.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <v-app-bar app color="orange lighten-1" flat dark>
+  <v-content>
+    <v-app-bar app color="orange lighten-1" flat dark fixed>
       <BackBtn />
       <v-toolbar-title>Page Not Found</v-toolbar-title>
     </v-app-bar>
@@ -8,13 +8,13 @@
     <div id="notFound">
       <br />
       <p>Oops we are really sorry but the page is not found...</p>
-      <p>You will be redirected home in 3 seconds...</p>
+      <p>You will be redirected back in 3 seconds...</p>
 
       <br />
       Go home now?
       <v-btn :to="{ name: 'home' }">Home</v-btn>
     </div>
-  </div>
+  </v-content>
 </template>
 
 <script>
@@ -22,18 +22,27 @@ import BackBtn from "@/components/BackBtn";
 
 export default {
   name: "notFound",
+  data() {
+    return {
+      // Timeout data object that can be referenced to clear if user navigates away before timeout
+      timeout: setTimeout(
+        router => {
+          // Navigate back to last location if possible. 2 as user's default home page may not be a blank tab
+          if (window.history.length > 2) router.back();
+          // Else, redirect back home
+          else router.push({ name: "home" });
+        },
+        3000,
+        this.$router
+      )
+    };
+  },
   components: {
     BackBtn
   },
-  mounted() {
-    setTimeout(
-      router => {
-        // Redirect back home
-        router.push({ name: "home" });
-      },
-      3000,
-      this.$router
-    );
+  beforeDestroy() {
+    // Prevent the timeout from still routing to home if user already navigated away themselves
+    clearTimeout(this.timeout);
   }
 };
 </script>
