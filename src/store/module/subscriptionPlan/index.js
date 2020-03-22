86886c64f3ded01aaa165bb8331e83261d1d6d83
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-22 22:05:02 +0800

    Refactored logic out of subscriptionPlan & updated UI: 1.  Refactored logic of subscriptionPlan out into the subscriptionPlan     vuex module, and added more to handle nextPlan and others. 2.  Updated SubscriptionPlan view to use these getters and state from the     vuex module. 3.  Added new UI for subscriptionPlan view to show current plan and next     plan if the user made a change in their plan.

--- src/store/module/subscriptionPlan/index.js
+++ src/store/module/subscriptionPlan/index.js
@@ -3,6 +3,7 @@
  */
 
 import initialState from "./initialState";
+import setter from "../../utils/setter";
 
 // @todo Remove mock data
 import mock from "../../mockData";
@@ -11,11 +12,27 @@ export default {
   namespaced: true,
   state: initialState(),
   mutations: {
-    setPlans(state, subscriptionPlans) {
-      state.subscriptionPlans = subscriptionPlans;
+    setter
+  },
+  getters: {
+    currentPlan(state) {
+      return state.subscriptionPlans.find(
+        plan => plan.id === state.currentPlanID
+      );
+    },
+    nextPlan(state) {
+      return state.subscriptionPlans.find(plan => plan.id === state.nextPlanID);
     }
   },
   actions: {
+    /**
+     * Initializing function for this module
+     * @function init
+     */
+    async init({ dispatch }) {
+      dispatch("getPlans");
+      dispatch("getCurrentPlanID");
+    },
     /**
      * Get list of available subscription plans from api
      * @function getPlans
@@ -24,7 +41,37 @@ export default {
       // @todo Replace with API call
       const plans = mock.subscriptionPlans;
 
-      commit("setPlans", plans);
+      commit("setter", ["subscriptionPlans", plans]);
+    },
+    /**
+     * Get ID of current plan
+     * @function getPlans
+     */
+    async getCurrentPlanID({ commit }) {
+      // @todo Replace with API call
+      const ID = 0;
+
+      commit("setter", ["currentPlanID", ID]);
+      commit("setter", ["nextPlanID", ID]);
+    },
+    /**
+     * Update the user's plan
+     * @function updatePlan
+     */
+    async updatePlan({ state, commit }, planID) {
+      // If user selects own plan, ignore selection
+      if (state.nextPlanID === planID) return;
+
+      if (confirm("Confirm change of Subscription Plan!")) {
+        // @todo Show loading bar before calling API for pessimistic UI
+
+        // Call logic to update the plan
+
+        console.log("Plan is updated");
+
+        // Pessimistic UI, show after network update is complete
+        commit("setter", ["nextPlanID", planID]);
+      }
     }
   }
 };
--- src/store/module/subscriptionPlan/initialState.js
+++ src/store/module/subscriptionPlan/initialState.js
@@ -4,6 +4,8 @@
  */
 export default function initialState() {
   return {
+    currentPlanID: undefined,
+    nextPlanID: undefined,
     subscriptionPlans: []
   };
 }
