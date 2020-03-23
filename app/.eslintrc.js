ec13dfd366890388093424299dd47566eefb2eb0
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-03-23 16:01:33 +0800

    Temporarily disabled eslint errors for "unused vars"

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
+    // @todo Remove after beta testing, useful for dev purposes.
+    "no-unused-vars": "off",
     // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
