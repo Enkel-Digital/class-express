641fe4bcf567667424cd54e91cc9d7ca3aeeb574
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-02 02:25:44 +0800

    Added a deepSetter for store's utils for testing.

--- app/src/store/utils/setter.js
+++ app/src/store/utils/setter.js
@@ -10,3 +10,23 @@ function setter(state, newState) {
 }
 
 export default setter;
+
+/**
+ * Deep setter
+ * @notice Yet to be tested.
+ */
+function deepSetter(state, newState) {
+  console.log("deepSetter", newState, state.user);
+  const path = newState.split(".");
+  const storeObj = state[path[0]];
+
+  let key = storeObj;
+  for (let i = 0; i < path.length; i++) {
+    key = key[path[i]];
+  }
+  key = newState[1];
+
+  state[path[0]] = storeObj; // Is this line needed?
+  console.log("deepSetter", newState, state.user);
+  return;
+}
