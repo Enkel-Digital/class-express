428416ed3186569bbf7ef04b653f5dea5b6e635d
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-04 01:40:56 +0800

    Updated UI of upcomingClasses view.

--- app/src/views/Upcoming.vue
+++ app/src/views/Upcoming.vue
@@ -13,35 +13,53 @@
         class="mx-auto mb-4"
         max-width="400"
         outlined
-        @click="showClass(clas.id)"
+        :ripple="false"
       >
-        <v-responsive>
+        <v-responsive @click="showClass(clas.id)">
           <v-img id="class-image" :src="clas.locationImage" />
-        </v-responsive>
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <div>
-            <v-card-title class="headline">{{ clas.className }}</v-card-title>
-            <v-card-subtitle>
-              {{ moment(clas.time).format("MMMM Do, h:mm") }} to
-              {{
-                moment(clas.time + Date.parse(clas.classLength)).format(
-                  "MMMM Do, h:mm"
-                )
-              }}
-            </v-card-subtitle>
 
-            <v-card-text class="text-left --primary">
-              <div>{{ clas.classProvider.name }}</div>
-              <div>{{ clas.location.address }}</div>
-            </v-card-text>
-          </div>
-        </div>
+          <v-list-item>
+            <div style="text-align: left;">
+              <v-card-title class="headline pl-0">
+                {{ clas.className }}
+              </v-card-title>
+              <!-- @todo Add a points box beside the class name -->
+              <!-- <p>7 points</p> -->
+
+              <v-list-item-subtitle style="font-weight: bold;">
+                {{ moment(clas.time).format("MMMM Do, h:mm") }} to
+                {{
+                  moment(clas.time + Date.parse(clas.classLength)).format(
+                    "MMMM Do, h:mm"
+                  )
+                }}
+              </v-list-item-subtitle>
+
+              <v-list-item-subtitle>
+                <div>{{ clas.classProvider.name }}</div>
+                <div>{{ clas.location.address }}</div>
+              </v-list-item-subtitle>
+            </div>
+          </v-list-item>
+        </v-responsive>
 
         <v-card-actions>
-          <!-- @todo Remove this button -->
-          <v-btn color="orange" text>Class Details</v-btn>
           <!-- @todo Extract out all share buttons to a common component -->
-          <v-btn color="orange" text>Share</v-btn>
+          <!-- @todo Implement PWA sharing and web share target code  -->
+          <v-btn icon>
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon @click="toggleFavourite(clas.id)">
+            <v-icon v-if="clas.favouritedClass" color="red">mdi-heart</v-icon>
+            <v-icon v-else>mdi-heart-outline</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-calendar-today</v-icon>
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-responsive>
