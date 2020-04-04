119a016fd6305628f0c212ad3fc9d179a319ad72
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-04 22:54:19 +0800

    Updated Favourites view UI: 1.  Updated UI to match the new UI of the upcomingClasses view. 2.  Added links to the classes image in mockData/classes.

--- src/store/mockData/classes.js
+++ src/store/mockData/classes.js
@@ -12,7 +12,10 @@ const classes = {
     location: {
       coordinates: "1.300649, 103.855453",
       address: "249B Victoria St, Bugis Village"
-    }
+    },
+    pictureSources: [
+      "https://tmw.com.sg/wp-content/uploads/2019/10/how-to-sharpen-your-guitar-skills-by-taking-classes-870x460.jpg"
+    ]
   },
   1: {
     id: 1,
@@ -27,7 +30,10 @@ const classes = {
     location: {
       coordinates: "1.302481, 103.855448",
       address: "117 Fidelio St"
-    }
+    },
+    pictureSources: [
+      "https://www.fetimes.co.kr/news/photo/201709/60017_41960_2138.jpg"
+    ]
   }
 };
 
