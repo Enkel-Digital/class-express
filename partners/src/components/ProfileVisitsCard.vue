342e607e4d0f8bc76b0de4e7d3a02e9922eb0a13
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-15 16:20:07 +0800

    Refactored cards out from Home view.

---
+++ partners/src/components/ProfileVisitsCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-md-4 col-12">
+    <!-- <v-card outlined :to="{name: 'bookings'}"> -->
+    <v-card outlined text>
+      <v-list-item>
+        <v-avatar color="indigo">
+          <v-icon color="white">
+            mdi-view-dashboard
+          </v-icon>
+        </v-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">
+            {{ "100" }}
+          </v-list-item-title>
+
+          <v-list-item-subtitle>
+            {{ "Profile visits (Month)" }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bookings-card"
+};
+</script>
--- partners/src/views/Home.vue
+++ partners/src/views/Home.vue
@@ -1,79 +1,34 @@
 <template>
   <v-content class="ClassDetails">
-    <div
-      class="v-responsive mx-auto overflow-visible"
-      style="max-width:1024px;"
-    >
-      <div class="container">
-        <v-toolbar>
-          <img src="@/assets/logo.png" style="height: inherit;" />
-          <v-toolbar-title>Your Dashboard</v-toolbar-title>
-        </v-toolbar>
+    <!-- <v-responsive class="mx-auto" style="max-width:1024px;"> -->
+    <v-container>
+      <v-toolbar>
+        <img src="@/assets/logo.png" style="height: inherit;" />
 
-        <div class="row">
-          <BookingsCard />
+        <v-toolbar-title>Your Dashboard</v-toolbar-title>
+      </v-toolbar>
 
-          <div class="col-md-4 col-12">
-            <div class="v-card v-card--outlined v-sheet theme--light">
-              <a
-                href="/en/components/buttons/"
-                tabindex="0"
-                class="v-list-item--doc v-list-item v-list-item--link theme--light primary--text"
-              >
-                <v-avatar color="indigo">
-                  <v-icon color="white">
-                    mdi-view-dashboard
-                  </v-icon>
-                </v-avatar>
-
-                <div class="v-list-item__content">
-                  <div class="v-list-item__title">
-                    <span>{{ "2069" }}</span>
-                  </div>
-                  <div class="v-list-item__subtitle">
-                    <span>{{ "Points Earned This Week" }}</span>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div class="col-md-4 col-12">
-            <div class="v-card v-card--outlined v-sheet theme--light">
-              <a
-                href="/en/components/buttons/"
-                tabindex="0"
-                class="v-list-item--doc v-list-item v-list-item--link theme--light primary--text"
-              >
-                <v-avatar color="indigo">
-                  <v-icon color="white">
-                    mdi-view-dashboard
-                  </v-icon>
-                </v-avatar>
-                <div class="v-list-item__content">
-                  <div class="v-list-item__title">
-                    <span>{{ "1470" }}</span>
-                  </div>
-                  <div class="v-list-item__subtitle">
-                    <span>{{ "Profile Visits" }}</span>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div class="row">
+        <BookingsCard />
+        <PointsEarnedCard />
+        <ProfileVisitsCard />
       </div>
-    </div>
+    </v-container>
+    <!-- </v-responsive> -->
   </v-content>
 </template>
 
 <script>
 import BookingsCard from "@/components/BookingsCard.vue";
+import PointsEarnedCard from "@/components/PointsEarnedCard.vue";
+import ProfileVisitsCard from "@/components/ProfileVisitsCard.vue";
 
 export default {
   name: "home",
   components: {
-    BookingsCard
+    BookingsCard,
+    PointsEarnedCard,
+    ProfileVisitsCard
   }
 };
 </script>
