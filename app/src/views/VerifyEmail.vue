2728c823b9e145aeebfb8079200c9306b9211a95
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-16 23:25:54 +0800

    Created verify email view: 1.  Created view to show after user completes signup. 2.  Mounted view as a public_only route.

---
+++ app/src/views/VerifyEmail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="signup">
+    <img alt="Login image" src="../assets/logo.png" width="360" height="360" />
+
+    <h2>Hello! Plz login after<br />verifying ur emailz</h2>
+
+    <br />
+    <br />
+
+    <v-btn color="orange" @click="resendVerificationEmail">
+      resend verification email
+    </v-btn>
+
+    <br />
+    <br />
+
+    <v-btn color="primary" :to="{ name: 'login' }">login</v-btn>
+  </div>
+</template>
+
+<script>
+import { auth } from "firebase";
+
+export default {
+  name: "verifyEmail",
+  data() {
+    return {
+      // Timeout data object that can be referenced to clear if user navigates away before timeout
+      timeout: setTimeout(
+        router => router.push({ name: "login" }), // Redirect user to login view
+        // 10000,
+        3000,
+        this.$router
+      )
+    };
+  },
+  methods: {
+    resendVerificationEmail() {
+      // Prevent timeout from routing to login if user request for a new verification email
+      clearTimeout(this.timeout);
+
+      if (confirm("Resend verification email?"))
+        auth().currentUser.sendEmailVerification();
+    }
+  },
+  beforeDestroy() {
+    // Prevent the timeout from routing to login if user already navigated away themselves
+    clearTimeout(this.timeout);
+  }
+};
+</script>
+
+<style scoped>
+img {
+  background-size: cover;
+}
+</style>
