5a287dc8f6e56ad6ef70387619f5bedf22b7854b
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-23 22:36:20 +0800

    Updated Welcome view UI and layout.

--- app/src/views/Welcome.vue
+++ app/src/views/Welcome.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="welcome">
-    <img alt="Login image" src="../assets/logo.png" width="360" height="370" />
-    <router-link class="button" to="/signup">Sign Up</router-link>
-    <router-link class="button" to="/login">Login</router-link>
-  </div>
+  <v-content id="welcome">
+    <v-img
+      eager
+      alt="Login image"
+      src="../assets/logo.png"
+      max-height="calc(100% - 1em)"
+      max-width="calc(100% - 1em)"
+      style="margin-top: calc(20%);"
+    />
+
+    <v-btn
+      :to="{ name: 'signup' }"
+      width="calc(100% - 5em)"
+      color="orange darken-1"
+      dark
+    >
+      Sign Up
+    </v-btn>
+
+    <br />
+    <br />
+
+    <v-btn
+      :to="{ name: 'login' }"
+      width="calc(100% - 5em)"
+      color="blue darken-2"
+      dark
+    >
+      Login
+    </v-btn>
+  </v-content>
 </template>
 
 <script>
@@ -11,36 +37,3 @@ export default {
   name: "welcome"
 };
 </script>
-
-<style scoped>
-.welcome {
-  display: flex;
-  flex-direction: column;
-
-  align-items: center;
-
-  position: absolute;
-  top: 10vh;
-  left: 50%;
-  right: 50%;
-  bottom: 10vh;
-}
-
-.button {
-  background-color: white;
-
-  max-width: 10em;
-  min-width: 8em;
-  padding: 0.6em;
-  margin-bottom: 1em;
-
-  font-size: 2em;
-
-  text-decoration: none;
-  color: black;
-
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 4em;
-}
-</style>
