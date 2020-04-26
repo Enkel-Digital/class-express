9f8aac6e45a76a5cd959e439d204b4b24235613b
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-26 18:24:16 +0800

    [CORE] Refactored db with new firebaseAdmin utils: 1.  Created a new firebaseAdmin utils module that wraps over the firebase     admin module and initializes it before exporting. 2.  Refactored db utils module to use the new firebaseAdmin module intead     of initializing it in the db module.

--- server/core/src/utils/db.js
+++ server/core/src/utils/db.js
@@ -4,28 +4,5 @@
  * @module DB singleton
  */
 
-const admin = require("firebase-admin");
-
-// Set GCP=true when using Google's infra, to use Google Application Default Credentials
-// Which are available by default so we do not need to get service key.
-if (process.env.GCP) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: "https://classes-ekd.firebaseio.com"
-  });
-} else {
-  // This is usually for the case when running locally and doing testing.
-  // Service Key json to be placed in root dir/
-  // Change this to be a module that reads from .env instead
-  const serviceAccountFile = require("path").join(__dirname, "../../serviceAccountKey.json");
-  const serviceAccount = require(serviceAccountFile);
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://classes-ekd.firebaseio.com"
-  });
-}
-
-const db = admin.firestore();
-
-module.exports = db;
+const admin = require("./firebaseAdmin");
+module.exports = admin.firestore();
