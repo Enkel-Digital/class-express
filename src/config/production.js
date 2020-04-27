19738894290b2a7b4ae2b6348bbdc32845f213c7
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-28 01:57:13 +0800

    Updated config to support different modes: 1.  Updated config module to a dir/ based module. 2.  Created 2 new config modules for production and development. 3.  Updated original config module to use env var to load config module     dynamically based on the App's build mode.

--- src/config/production.js
+++ src/config/production.js
@@ -1,11 +1,11 @@
 /**
+ * Config for production
  * Config options module to be imported by the other modules to use for consistency.
  */
 
 // Using this instead of es6 module syntax for easier export syntax + import destructuring.
 module.exports = {
   // URL cannot have ending slash "/"
-  // apiUrl: "https://api.classexpress.com",
-  apiUrl: "http://localhost:3000",
-  version: "0.0.1"
+  // @todo To update URL once domain is fixed
+  apiUrl: "https://api.enkeldigital.com"
 };
