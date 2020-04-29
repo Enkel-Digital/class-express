027c277ab6563902a56318c40529e7306c411056
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-04-30 02:31:59 +0800

    [CORE] Created subscription route and mounted it.

--- server/core/src/routes/index.js
+++ server/core/src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const defaultRoutes = require("./default");
 const userRoutes = require("./users");
+const subscriptionRoutes = require("./subscription");
 
 // Mount all the routes onto their respective base routes
 router.use("/", defaultRoutes);
 router.use("/user", userRoutes);
+router.use("/subscription", subscriptionRoutes);
 
 module.exports = router;
---
+++ server/core/src/routes/subscription.js
@@ -0,0 +1,38 @@
+/**
+ * Express Router for subscription related routes
+ * Mounted on /subscription
+ * @author JJ
+ * @module Subscription routes
+ */
+
+const express = require("express");
+const router = express.Router();
+const db = require("../utils/db");
+const auth = require("../middleware/auth");
+
+const createLogger = require("@lionellbriones/logging").default;
+const logger = createLogger("routes:subscription");
+
+/**
+ * Get subscriptionplans
+ * @name GET /subscription/plans/all
+ * @function
+ * @returns {object} List of subscription plans
+ */
+router.get("/plans/all", auth, async (req, res) => {
+  try {
+    // Sort them by id asc
+    const subscriptionPlansSnapshot = await db
+      .collection("subscriptionPlans")
+      .orderBy("id")
+      .get();
+    const subscriptionPlans = subscriptionPlansSnapshot.docs.map(doc => doc.data());
+
+    res.json({ success: true, subscriptionPlans });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+module.exports = router;
