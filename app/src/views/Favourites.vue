a6d92068f912636d9cf7d113a5b975c28c2034c7
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-05-07 23:10:17 +0800

    Refactored favourites view into components: 1.  Refactored favourites view to FavouriteClasses and FavouritePartners     components to split up the logic. 2.  Added tab items to Favourites view to swipe between the 2 components.

--- app/src/views/Favourites.vue
+++ app/src/views/Favourites.vue
@@ -19,95 +19,42 @@
           Does not require an active item. Using v-tab with router navigation
           Using replace attribute to use vue router replace when switching between tabs
         -->
-        <v-tabs centered grow>
-          <v-tab replace append>Classes</v-tab>
+        <v-tabs v-model="tab" centered grow>
           <v-tab replace append>Partners</v-tab>
+          <v-tab replace append>Classes</v-tab>
         </v-tabs>
       </template>
     </v-app-bar>
 
     <br />
 
-    <v-responsive v-if="favouriteClasses.length">
-      <v-card
-        v-for="clas in favouriteClasses"
-        :key="clas.id"
-        class="mx-auto mb-4"
-        max-width="calc(100% - 1.6em)"
-        outlined
-        :ripple="false"
-      >
-        <v-responsive
-          @click="
-            $router.push({ name: 'ClassDetails', params: { classID: clas.id } })
-          "
-        >
-          <!-- @todo Change to a image carousel -->
-          <v-img id="class-image" :src="clas.pictureSources[0]" />
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <FavouritePartners />
+      </v-tab-item>
 
-          <v-list-item>
-            <div style="text-align: left;">
-              <v-card-title class="headline pl-0">
-                {{ clas.name }}
-              </v-card-title>
-              <!-- @todo Add a points box beside the class name -->
-              <!-- <p>7 points</p> -->
-
-              <v-list-item-subtitle
-                :set="(partner = getPartner(clas.partnerID))"
-              >
-                <div style="font-weight: bold;">
-                  {{ partner.name }}
-                </div>
-                <div>
-                  {{
-                    clas.location
-                      ? clas.location.address
-                      : partner.location.address
-                  }}
-                </div>
-              </v-list-item-subtitle>
-            </div>
-          </v-list-item>
-        </v-responsive>
-
-        <v-card-actions>
-          <v-spacer />
-
-          <!-- Change this to a remove icon only. Cos dont need to toggle, here means confirm favourites already -->
-          <v-btn icon @click="toggleFavourite(clas.id)">
-            <v-icon color="red">mdi-heart</v-icon>
-          </v-btn>
-
-          <!-- @todo Extract out all share buttons to a common component -->
-          <!-- @todo Implement PWA sharing and web share target code  -->
-          <v-btn icon>
-            <v-icon>mdi-share-variant</v-icon>
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-responsive>
-
-    <!-- @todo Add copywriting for users to add classes if they have no favourite classes -->
-    <v-responsive v-else>
-      Add classes to your favourites now!
-    </v-responsive>
+      <v-tab-item>
+        <FavouriteClasses />
+      </v-tab-item>
+    </v-tabs-items>
   </v-content>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import FavouriteClasses from "@/components/favourites/Classes.vue";
+import FavouritePartners from "@/components/favourites/Partners.vue";
 
 export default {
   name: "favourites",
-  computed: {
-    ...mapGetters("classes", ["favouriteClasses"])
+  components: {
+    FavouriteClasses,
+    FavouritePartners
   },
-  methods: {
-    ...mapActions("classes", ["toggleFavourite"]),
-    getPartner(partnerID) {
-      return this.$store.state.classes.partners[partnerID];
-    }
+  data() {
+    return {
+      // By default show the classes tab on the left
+      tab: 1
+    };
   }
 };
 </script>
