63df89af7f28f8fdc112a1a6b4962ac886e7a97b
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-05-08 21:58:44 +0800

    Updated mockData and fixed favourites routing: 1.  Updated address in mockData and added a new class. 2.  Fixed routing for favourite partners cards to route to partners view     rather than ClassDetails.

--- src/store/mockData/classes.js
+++ src/store/mockData/classes.js
@@ -17,8 +17,8 @@ const partners = {
     id: 1,
     name: "Tampines CC",
     location: {
-      coordinates: "1.302481, 103.855448",
-      address: "117 Fidelio St"
+      coordinates: "1.348979, 103.935787",
+      address: "5 Tampines Ave 3"
     },
     description:
       "Located in the Heartlands of Tampines, we offer a wide variety of classes for our residents.",
@@ -85,6 +85,19 @@ const classes = {
     partnerID: 2,
     pictureSources: [
       "https://pickupmusic.com/wp-content/uploads/2020/01/Ichka-web-3-1775x2048.jpg"
+    ]
+  },
+  3: {
+    id: 3,
+    name: "Basic Cooking",
+    points: 3,
+    time: Date.now() + 1000000,
+    length: 150, // Store classLength in minutes can show otherwise in hours as needed
+    description:
+      "Want to get started in the magical world of cooking? Well join our class to learn more and get ready to be amazed.",
+    partnerID: 1,
+    pictureSources: [
+      "https://d2ga8dje9bus38.cloudfront.net/0QTxUDcDSYaU0951YubV_verlocal_cooking_basics_class_workshop_in_oakland_900_600.jpg"
     ]
   }
 };
