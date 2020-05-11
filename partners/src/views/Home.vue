76c95a98c63fc044a0258d16b19d33dba4a791b5
Jessica Jacelyn <jessicajacelyn@gmail.com>
2020-05-12 05:06:18 +0800

    Created WeeklyEarningsChart component with ChartJS: 1.  Added vue-chartjs dependency 2.  Built the new component with mock data

--- partners/src/views/Home.vue
+++ partners/src/views/Home.vue
@@ -3,8 +3,6 @@
     <!-- <v-responsive class="mx-auto" style="max-width:1024px;"> -->
     <v-container>
       <v-toolbar>
-        <img src="@/assets/logo.png" style="height: inherit;" />
-
         <v-toolbar-title>Your Dashboard</v-toolbar-title>
       </v-toolbar>
 
@@ -16,7 +14,12 @@
 
       <br />
       <div class="row">
-        <DashboardEarningsSparkline />
+        <WeeklyEarningsChart
+          width="800"
+          height="300"
+          :chartdata="chartdata"
+          :options="options"
+        />
         <PastWeekEarningComparison />
       </div>
     </v-container>
@@ -28,7 +31,7 @@
 import BookingsCard from "@/components/BookingsCard.vue";
 import PointsEarnedCard from "@/components/PointsEarnedCard.vue";
 import ProfileVisitsCard from "@/components/ProfileVisitsCard.vue";
-import DashboardEarningsSparkline from "@/components/DashboardEarningsSparkline.vue";
+import WeeklyEarningsChart from "@/components/WeeklyEarningsChart.vue";
 import PastWeekEarningComparison from "@/components/PastWeekEarningComparisonCard.vue";
 
 export default {
@@ -37,8 +40,45 @@ export default {
     BookingsCard,
     PointsEarnedCard,
     ProfileVisitsCard,
-    DashboardEarningsSparkline,
+    WeeklyEarningsChart,
     PastWeekEarningComparison,
   },
+  data: () => ({
+    chartdata: {
+      labels: [
+        "May 1",
+        "May 2",
+        "May 3",
+        "May 4",
+        "May 5",
+        "May 6",
+        "May 7",
+        "May 8",
+      ],
+      datasets: [
+        {
+          label: "Points Earned",
+          backgroundColor: "#78909C",
+          borderColor: "#455A64",
+          borderWidth: "2",
+          fill: false,
+          data: [60, 50, 60, 90, 50, 43, 54, 70],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  }),
 };
 </script>
