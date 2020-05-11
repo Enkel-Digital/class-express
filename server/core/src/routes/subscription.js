115e320cf6368bc47957e0315f8cf32f4d388c94
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-05-12 02:03:27 +0800

    [CORE] Implemented get users' subscription plans.

--- server/core/src/routes/subscription.js
+++ server/core/src/routes/subscription.js
@@ -9,9 +9,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("../utils/db");
 const auth = require("../middleware/auth");
+const onlyOwnResource = require("../middleware/onlyOwnResource");
 
 const createLogger = require("@lionellbriones/logging").default;
 const logger = createLogger("routes:subscription");
+
+// Default points object for new user
+const defaultPlansObject = {
+  currentPlanID: null,
+  nextPlanID: null
+};
 
 /**
  * Get subscriptionplans
@@ -24,6 +31,7 @@ router.get("/plans/all", auth, async (req, res) => {
     // Sort them by id asc
     const subscriptionPlansSnapshot = await db
       .collection("subscriptionPlans")
+      // .where("available", "==", true) // Allow us to run campaigns for subscription plans
       .orderBy("id", "desc")
       .get();
     const subscriptionPlans = subscriptionPlansSnapshot.docs.map(doc => doc.data());
@@ -32,6 +40,43 @@ router.get("/plans/all", auth, async (req, res) => {
   } catch (error) {
     logger.error(error);
     res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+/**
+ * Get users' subscription plan
+ * @name GET /subscription/:userID
+ * @function
+ * @returns {object} IDs of users' subscription plans
+ */
+router.get("/:userID", auth, onlyOwnResource, async (req, res) => {
+  try {
+    const { userID } = req.params;
+
+    const plansDoc = await db
+      .collection("userPlans")
+      .doc(userID)
+      .get();
+
+    let plans = plansDoc.data();
+
+    // If no plans doc, means first time using app
+    if (!plans) {
+      // Create document with user's email and default plans object
+      // Not awaiting for set to complete before replying user.
+      // @todo Might cause an issue if this fails
+      db.collection("userPlans")
+        .doc(userID)
+        .set(defaultPlansObject);
+
+      // Set user's plans to the default plans object that was just set to DB
+      plans = defaultPlansObject;
+    }
+
+    res.json({ success: true, plans });
+  } catch (error) {
+    logger.error(error);
+    res.status(error.code ? error.code : 500).json({ success: false, error: error.message });
   }
 });
 
