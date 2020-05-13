b0dd94f83a32897f44efd35d49df185c27ad1d1c
Jaime <jaimeloeuf@gmail.com>
2020-05-13 21:36:01 +0800

    Created simple utils wrapper module for loader: 1.  Created a utils module to wrap over the loader vuex module to make     it easier to dispatch their actions. 2.  Updated subscriptionPlan module's updatePlan action to use the new     loader wrapper.

---
+++ app/src/store/utils/loader.js
@@ -0,0 +1,16 @@
+/**
+ * Simple wrapper over actions from the loader module
+ */
+
+import store from "@/store";
+
+export default {
+  async new(loaderRequest) {
+    return store.dispatch("loader/new", loaderRequest, {
+      root: true
+    });
+  },
+  async clear(loaderRequestID) {
+    store.dispatch("loader/clear", loaderRequestID, { root: true });
+  }
+};
