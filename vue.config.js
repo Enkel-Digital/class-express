91da874fdb8232ace1238e73e638ef1096700f93
Jaime <jaimeloeuf@gmail.com>
2020-06-03 01:35:02 +0800

    Bug fixes related to loader plugin: 1.  Updated vue config file to update webpack config to allow local npm     package testing by preventing symbolic link resolving. 2.  Updated package name as it should not be a cli plugin, which in turn     causes the cli to not build the project as the loader plugin was treated     as a cli plugin even though it is not. Issue is solved by updating the     package and republishing under a different name without vue-cli-plugin     prefix. 3.  Updated use of the plugin in main.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  // Used when using local npm packages (components/plugins) for testing before they are published.
+  chainWebpack: (config) => config.resolve.symlinks(false),
   configureWebpack: {
     module: {
       noParse: /\.md$/,
