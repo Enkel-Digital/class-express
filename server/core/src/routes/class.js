ecf787401605aec2099c6a9007ccd47332cd6a55
Jaime <jaimeloeuf@gmail.com>
2020-06-04 20:51:27 +0800

    [CORE] Implemented GET class details API.

--- server/core/src/routes/class.js
+++ server/core/src/routes/class.js
@@ -1,5 +1,5 @@
 /**
- * Express Router for class related routes
+ * Express Router for class related routes like reserving, cancelling a class and more
  * Mounted on /class
  * @author JJ
  * @module Class routes
@@ -8,6 +8,31 @@
 const express = require("express");
 const db = require("../utils/db");
 const router = express.Router();
+
+const createLogger = require("@lionellbriones/logging").default;
+const logger = createLogger("routes:users");
+
+/**
+ *
+ * Get class details
+ * @name GET /class/details/:classID
+ * @function
+ * @returns {object} Class object
+ */
+router.get("/details/:classID", async (req, res) => {
+  try {
+    const { classID } = req.params;
+
+    const snapshot = await db.collection("classes").doc(classID).get();
+    const classObject = snapshot.data();
+
+    if (classObject) res.json({ success: true, class: classObject });
+    else res.status(404).json({ success: false, error: "Invalid classID" });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 /**
  * Get class object
