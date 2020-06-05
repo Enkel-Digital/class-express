5a6973e53da054bf78955ea2b79e2bcd213e47b6
Jaime <jaimeloeuf@gmail.com>
2020-06-05 18:01:04 +0800

    Remove unused mockdata and updated dependencies.

--- app/src/store/mockData/classes.js
+++ app/src/store/mockData/classes.js
@@ -41,67 +41,6 @@ const partners = {
   },
 };
 
-const classes = {
-  0: {
-    id: 0,
-    name: "Basic Guitar",
-    points: 5,
-    time: Date.now(),
-    length: 60, // Store classLength in minutes can show otherwise in hours as needed
-    description:
-      "Basic guitar lessons to help you get started with this wonderful musical instrument! This class covers all the basics from score reading to strumming techniques.",
-    partnerID: 0,
-    // By default class is assumed to be held at partners venue.
-    // However, class can have an optional location to override the default one
-    location: {
-      coordinates: "1.3006954, 103.84475",
-      address: "Orchard road, Plaza Singapura",
-    },
-    pictureSources: [
-      "https://tmw.com.sg/wp-content/uploads/2019/10/how-to-sharpen-your-guitar-skills-by-taking-classes-870x460.jpg",
-    ],
-  },
-  1: {
-    id: 1,
-    name: "Advanced Cooking",
-    points: 6,
-    time: Date.now() + 1000000,
-    length: 150, // Store classLength in minutes can show otherwise in hours as needed
-    description:
-      "Like cooking but always feel like you are missing a magical ingredient? Well join our class to learn more and get ready to be amazed.",
-    partnerID: 1,
-    pictureSources: [
-      "https://www.fetimes.co.kr/news/photo/201709/60017_41960_2138.jpg",
-    ],
-  },
-  2: {
-    id: 2,
-    name: "Advanced Guitar",
-    points: 8,
-    time: Date.now() + 1000000,
-    length: 60, // Store classLength in minutes can show otherwise in hours as needed
-    description:
-      "Advance guitar lessons taught be the legendary Ichika Mo.<br />Will be going through advanced music scores and includes 1 on 1 trainings for the students, alongside a chance to practice in front of a live audience",
-    partnerID: 2,
-    pictureSources: [
-      "https://pickupmusic.com/wp-content/uploads/2020/01/Ichka-web-3-1775x2048.jpg",
-    ],
-  },
-  3: {
-    id: 3,
-    name: "Basic Cooking",
-    points: 3,
-    time: Date.now() + 1000000,
-    length: 150, // Store classLength in minutes can show otherwise in hours as needed
-    description:
-      "Want to get started in the magical world of cooking? Well join our class to learn more and get ready to be amazed.",
-    partnerID: 1,
-    pictureSources: [
-      "https://d2ga8dje9bus38.cloudfront.net/0QTxUDcDSYaU0951YubV_verlocal_cooking_basics_class_workshop_in_oakland_900_600.jpg",
-    ],
-  },
-};
-
 const reviews = {
   3: {
     classID: 3,
@@ -213,7 +152,6 @@ const pastClassesID = {
 };
 
 module.exports = {
-  classes,
   partners,
   reviews,
   schedule,
