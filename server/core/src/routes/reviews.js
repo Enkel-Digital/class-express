ce84e1d6782c62d07ba7673433a1617d96fb29a2
Jaime <jaimeloeuf@gmail.com>
2020-06-14 02:53:26 +0800

    [CORE] Implemented API for writing class reviews.

--- server/core/src/routes/reviews.js
+++ server/core/src/routes/reviews.js
@@ -7,6 +7,7 @@
 
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const firebase = require("firebase-admin");
 const db = require("../utils/db");
 
@@ -56,5 +57,45 @@ router.get("/class/:classID", async (req, res) => {
 //     res.status(500).json({ success: false, error: error.message });
 //   }
 // });
+
+/**
+ * Leave a new review after user have attended a class
+ * @name POST /reviews/new/:classID
+ * @function
+ * @param {String} userID
+ * @param {String} description
+ * @param {Number} points
+ * @returns {object} Success indicator
+ */
+router.post("/new/:classID", auth, express.json(), async (req, res) => {
+  try {
+    const { classID } = req.params;
+    const { description, points } = req.body;
+    const userID = req.body.userID.toLowerCase();
+
+    // Get the current number of reviews + 1?
+    // @todo Might cause issues when dealing with multiple concurrent writes, might be better to use a random ID for the map key
+    const count = ++(await db.collection("reviews").doc(classID).get()).data()
+      .numberOfReviews;
+
+    await db
+      .collection("reviews")
+      .doc(classID)
+      .update({
+        numberOfReviews: firebase.firestore.FieldValue.increment(1),
+        [`userReviews.${count}`]: {
+          userID, // @todo To change this as userID as email can be updated
+          description,
+          points,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+      });
+
+    res.status(201).json({ success: true });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 module.exports = router;
