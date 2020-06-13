235d63e4850d74e7b356ad5759215e6be5b7f149
Jaime <jaimeloeuf@gmail.com>
2020-06-13 20:44:33 +0800

    Minor UI updates to NavBar and Profile view.

--- src/components/BottomNavBar.vue
+++ src/components/BottomNavBar.vue
@@ -1,28 +1,32 @@
 <template>
+  <!--
+    Temporarily hiding all the words for the icons to make it more usable on smaller devices
+  -->
+
   <!-- Bottom navigation bar stays the same across the screens -->
-  <v-bottom-navigation app height>
+  <v-bottom-navigation app height grow hide-on-scroll color="orange darken-3">
     <v-btn :to="{ name: 'home' }">
-      <span>Home</span>
+      <!-- <span>Home</span> -->
       <v-icon>mdi-home</v-icon>
     </v-btn>
 
     <v-btn :to="{ name: 'explore' }">
-      <span>Explore</span>
+      <!-- <span>Explore</span> -->
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
     <v-btn :to="{ name: 'favourites' }">
-      <span>Favourites</span>
+      <!-- <span>Favourites</span> -->
       <v-icon>mdi-heart</v-icon>
     </v-btn>
 
     <v-btn :to="{ name: 'upcoming' }">
-      <span>Upcoming</span>
+      <!-- <span>Upcoming</span> -->
       <v-icon>mdi-calendar-today</v-icon>
     </v-btn>
 
     <v-btn :to="{ name: 'profile' }">
-      <span>Profile</span>
+      <!-- <span>Profile</span> -->
       <v-icon>mdi-account-circle</v-icon>
     </v-btn>
   </v-bottom-navigation>
