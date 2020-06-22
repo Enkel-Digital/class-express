94a9814b89fd1835d0cd04b7db20bfb78f70c5c5
Jaime <jaimeloeuf@gmail.com>
2020-06-22 22:27:56 +0800

    [CORE] Updated onlyOwnResource middleware checks:     Updated the check to allow userEmail to be passed in, instead of     userID as we transition away from using user's email as their UUID.

--- server/core/src/middleware/onlyOwnResource.js
+++ server/core/src/middleware/onlyOwnResource.js
@@ -8,19 +8,23 @@
  * This middleware enforces that users can only access their own resources
  */
 module.exports = async function onlyOwnResource(req, res, next) {
-  const { userID } = req.params;
+  // @todo To migrate to only userEmail and only userID, where userID will no longer be email
+  const { userID, userEmail } = req.params;
 
   /**
    * Only if route did not use auth middleware but used this middleware will this fail,
    * else if the auth middleware did not attach these values to use downstream.
    */
   if (!req.authenticatedUser)
-    return res.status(500).json({
+    return res.status(401).json({
       success: false,
       error: "Missing authenticated user data from token",
     });
 
-  if (userID !== req.authenticatedUser.email)
+  if (
+    req.authenticatedUser.email !== userID &&
+    req.authenticatedUser.email !== userEmail
+  )
     return res.status(403).json({
       success: false,
       error: "Forbidden",
