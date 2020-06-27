4d0a51fa455cacf79ca67b48f067f77f757aefd3
Jaime <jaimeloeuf@gmail.com>
2020-06-28 01:01:25 +0800

    [DB/sql_management] Fixed teardown with connections:     Added DB connection teardown to teardown script to make it work even     when there are clients connected to the DB preventing dropping it.

--- server/DB/sql_management/teardown.js
+++ server/DB/sql_management/teardown.js
@@ -19,6 +19,28 @@ require("dotenv").config();
     config.connection.database = null; // change to null to connect to default DB to modify the actual targetted DB
     knex = require("knex")(config);
 
+    // Teardown all current DB connections before DB can be dropped
+    // https://www.postgresqltutorial.com/postgresql-drop-database/
+    const DB_pids = await knex.raw(`
+      SELECT
+          pid
+      FROM
+          pg_stat_activity
+      WHERE
+          "datname" = '${dbName}';
+    `);
+
+    for (const row of DB_pids.rows) {
+      await knex.raw(`
+        SELECT
+          pg_terminate_backend (${row.pid})
+        FROM
+          pg_stat_activity
+        WHERE
+          "datname" = '${dbName}';
+      `);
+    }
+
     // Drop the database only if it exists
     // DB name is case sensitive thus in double quotes
     await knex.raw(`DROP DATABASE IF EXISTS "${dbName}";`);
