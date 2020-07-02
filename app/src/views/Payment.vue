66be08d1c924bd935f54f94d0ecae6264cf93ec6
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-07-02 21:42:46 +0800

    Update payment page with vue card

--- app/src/views/Payment.vue
+++ app/src/views/Payment.vue
@@ -1,38 +1,144 @@
 <template>
-  <section class="row payment-form">
-    <h5 class="#e0e0e0 grey lighten-4">
-      Payment Method
-      <span class="right">${{ amount }}</span>
-    </h5>
+  <div>
+    <v-card class="mx-auto" max-width="344">
+      <v-card-text>
+        <p class="display-1 text--primary">
+          Payment Method
+        </p>
+        <p>Subscription Plan {{ $route.params.planID }}</p>
+        <div class="text--primary error red center-align white-text">
+          {{ stripeValidationError }}
+        </div>
 
-    <div class="error red center-align white-text">
-      {{ stripeValidationError }}
-    </div>
+        <div class="col s12 card-element">
+          <label>Card Number</label>
+          <div id="card-number-element" class="input-value"></div>
+        </div>
 
-    <div class="col s12 card-element">
-      <label>Card Number</label>
-      <div id="card-number-element" class="input-value"></div>
-    </div>
+        <div class="col s6 card-element">
+          <label>Expiry Date</label>
+          <div id="card-expiry-element"></div>
+        </div>
 
-    <div class="col s6 card-element">
-      <label>Expiry Date</label>
-      <div id="card-expiry-element"></div>
-    </div>
-
-    <div class="col s6 card-element">
-      <label>CVC</label>
-      <div id="card-cvc-element"></div>
-    </div>
-
-    <div class="col s12 place-order-button-block">
-      <button class="btn col s12 #e91e63 pink" @click="placeOrderButtonPressed">
-        PlaceOrder
-      </button>
-    </div>
-  </section>
+        <div class="col s6 card-element">
+          <label>CVC</label>
+          <div id="card-cvc-element"></div>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          class="col s12"
+          color="deep-purple centre"
+          @click="placeOrderButtonPressed"
+        >
+          PlaceOrder
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
+<script src="https://js.stripe.com/v3/"></script>
+<script>
+// import { loadStripe } from "@stripe/stripe-js";
 
-<script></script>
+//import CardForm from "../components/CardForm.vue";
+export default {
+  components: {},
+
+  data() {
+    return {
+      stripe: null,
+      cardNumberElement: null,
+      cardExpiryElement: null,
+      cardCVCElement: null,
+      amount: 1,
+      stripeValidationError: null,
+
+      cardName: "",
+      cardNumber: "",
+      cardMonth: "",
+      cardYear: "",
+      cardCvc: "",
+      expiryDate: "",
+    };
+  },
+  created() {
+    //this.stripe = loadStripe(`pk_test_TYooMQauvdEDq54NiTphI7jx`);
+  },
+
+  mounted() {
+    // const callstripe = (async () => {
+    //   this.stripe = await loadStripe(
+    //     "pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD"
+    //   );
+    // })();
+
+    // this.stripe = loadStripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+    // this.createAndMountFormElement();
+
+    // this.$loadScript("https://js.stripe.com/v3/")
+    //   .then((Stripe) => {
+    //     // Script is loaded, do something
+    //     this.stripe = Stripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+    //     this.createAndMountFormElement();
+    //   })
+    //   .catch(() => {
+    //     // Failed to fetch script
+    //     console.log("error");
+    //   });
+
+    let stripeaScript = document.createElement("script");
+    stripeaScript.setAttribute("src", "https://js.stripe.com/v3/");
+    stripeaScript.async = true;
+    document.head.appendChild(stripeaScript);
+
+    this.stripe = Stripe("pk_test_LIc2NCzFeOD5ng6VrGwNE8Dx00Z67P4mCD");
+    this.createAndMountFormElement();
+  },
+  methods: {
+    createAndMountFormElement() {
+      var elements = this.stripe.elements();
+
+      console.log(elements);
+      this.cardNumberElement = elements.create("cardNumber");
+      this.cardNumberElement.mount("#card-number-element");
+
+      this.cardExpiryElement = elements.create("cardExpiry");
+      this.cardExpiryElement.mount("#card-expiry-element");
+
+      this.cardCvcElement = elements.create("cardCvc");
+      this.cardCvcElement.mount("#card-cvc-element");
+
+      this.cardNumberElement.on("change", this.setValidationError);
+      this.cardExpiryElement.on("change", this.setValidationError);
+      this.cardCVCElement.on("change", this.setValidationError);
+    },
+    setValidationError(event) {
+      this.stripeValidationError = event.error ? event.error.message : "";
+    },
+    placeOrderButtonPressed() {
+      this.stripe.createToken(this.cardNumberElement).then((result) => {
+        if (result.error) {
+          this.stripeValidationError = result.error.message;
+        } else {
+          var stripeObject = {
+            amount: this.amount,
+            source: result.token,
+          };
+
+          console.log(stripeObject);
+        }
+      });
+    },
+    updateCardNumber(val) {},
+    updateCardName(val) {},
+    updateCardMonth(val) {},
+    updateCardYear(val) {},
+    updateCardCvv(val) {},
+  },
+};
+</script>
 
 <style>
 .payment-form {
