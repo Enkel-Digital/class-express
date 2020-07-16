a87bdee91b7ec80cdb55859880b1d0ce106518ef
Jaime <jaimeloeuf@gmail.com>
2020-07-16 19:04:37 +0800

    [partner] Implemented GET and ADD class tags API.

--- server/partners/src/routes/index.js
+++ server/partners/src/routes/index.js
@@ -10,6 +10,7 @@ router.use("/error", require("./error"));
 router.use("/class", require("./class"));
 router.use("/partner", require("./partner"));
 router.use("/user", auth, require("./users"));
+router.use("/tags/class", auth, require("./classTags"));
 router.use("/favourites", auth, require("./favourites"));
 router.use("/subscription", require("./subscription"));
 router.use("/points", auth, require("./points"));
