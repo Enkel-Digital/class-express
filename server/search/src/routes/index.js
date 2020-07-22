45b037f7820d8be3c64627ba33bb235decb79aae
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-07-23 00:07:14 +0800

    [search] algolia setup

--- server/search/src/routes/index.js
+++ server/search/src/routes/index.js
@@ -1,10 +1,13 @@
 /** @notice Parent router where all other routers are mounted onto. */
 const express = require("express");
 const router = express.Router();
+const algoliaIndex = require("../utils/algolia");
 
 const auth = require("../middleware/auth");
 
 // Mount all the routes onto their respective base routes
 router.use("/", require("./default"));
+
+router.post("/new/bulk", express.json());
 
 module.exports = router;
