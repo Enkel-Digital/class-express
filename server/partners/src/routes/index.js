5061de434536112ff979252cb14de34bb93d14cd
Jaime <jaimeloeuf@gmail.com>
2020-07-26 02:44:04 +0800

    [partner] Removed unused error API.

--- server/partners/src/routes/index.js
+++ server/partners/src/routes/index.js
@@ -6,7 +6,6 @@ const auth = require("../middleware/auth");
 
 // Mount all the routes onto their respective base routes
 router.use("/", require("./default"));
-router.use("/error", require("./error"));
 router.use("/class", require("./class"));
 router.use("/partner", require("./partner"));
 router.use("/user", auth, require("./users"));
