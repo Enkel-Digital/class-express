bca70686212cebda5f494cc2476dd3da51695d17
Jaime <jaimeloeuf@gmail.com>
2020-07-28 23:56:33 +0800

    Updated login view:     Updated it to use error plugin and check for email verification.

--- partners/src/views/Login.vue
+++ partners/src/views/Login.vue
@@ -25,7 +25,6 @@
         required
       />
 
-      <p class="error">{{ error_msg }}</p>
       <button @click="login">Login</button>
       <br />
       <button id="back-btn" @click="back">Back</button>
@@ -45,9 +44,13 @@ import "firebase/auth";
 function error_msg(err) {
   switch (err.code) {
     case "auth/wrong-password":
-      return "Invalid password or email.";
+      return "Invalid password or email!";
     case "auth/network-request-failed":
       return "Oops, check your internet connection!";
+    case "auth/user-not-found":
+      return "Sorry but you dont have an account with us 😭<br />Signup now!";
+    case "email/no-verify":
+      return "Email not verified.<br />Please verify before trying again";
     default:
       return "Something went wrong! Please try again.";
   }
@@ -59,7 +62,6 @@ export default {
     return {
       email: "",
       password: "",
-      error_msg: "",
     };
   },
   methods: {
@@ -67,24 +69,53 @@ export default {
       this.$router.push({ name: "welcome" });
     },
     async login() {
-      try {
-        // @todo Show loading screen while authenticating and loading user data
+      // Show loading screen before login logic executes
+      const loaderRequestID = this.$loader.new();
 
+      try {
         // eslint-disable-next-line no-unused-vars
         const usr = await firebase
           .auth()
           .signInWithEmailAndPassword(this.email, this.password);
 
-        // Await for async dispatch to ensure app only starts when user info is all available.
-        await this.$store.dispatch("getUserDetails");
+        if (!firebase.auth().currentUser.emailVerified) {
+          // Signout the user and redirect to verifyEmail view
+          await firebase.auth().signOut();
 
-        await this.$store.dispatch("init");
+          // Throw new error with pre-defined code to get the right error_msg
+          const error = new Error();
+          error.code = "email/no-verify";
+          throw error;
+        } else {
+          // Await for async dispatch to ensure app only starts after user info is available.
+          await this.$store.dispatch("getUserDetails", this.email);
+          this.$store.dispatch("init");
 
-        // Route to the user's home page, after login
-        this.$router.replace({ name: "home", params: { user: name } });
+          // Route to the user's home page, after login
+          this.$router.replace({ name: "home", params: { user: name } });
+        }
       } catch (error) {
-        // Set the message into the error box to show user the error
-        this.error_msg = error_msg(error);
+        // If there is an error but user is somehow logged in, sign user out to try again
+        if (firebase.auth().currentUser) await firebase.auth().signOut();
+
+        // @todo Instead of changing route, perhaps, show via ErrorDialog and let user know they need to verify their email
+        // @todo Then use internal notification dialog to show email verification after user signup.
+        if (error.code === "email/no-verify")
+          return this.$router.replace({
+            name: "verify-email",
+            params: { emailAddress: this.email },
+          });
+
+        // Create new user error and show with ErrorDialog
+        const userError = this.$error.createError(
+          this.$error.ERROR.level.RETRY,
+          this.$error.ERROR.custom("Authentication Failed", error_msg(error))
+        );
+        this.$error.new(userError);
+      } finally {
+        // Remove loader after login logic completes regardless of whether login failed or succeeded
+        // Inside finally to ensure execution even if catch block was ran
+        this.$loader.clear(loaderRequestID);
       }
     },
   },
@@ -116,10 +147,6 @@ button {
   border-style: solid;
   border-width: thin;
   border-radius: 4em;
-}
-
-.error {
-  margin-top: 1em;
 }
 
 #back-btn {
