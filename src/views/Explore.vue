89798d8e7f5b42b0e8c1ec9b31ced1495a18bfc1
Kong Wei <kongwei83@hotmail.com>
2020-07-29 16:38:29 +0800

    remove unused codes

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,9 +1,7 @@
 <template>
   <v-content id="explore">
     <v-app-bar app flat color="white">
-      <v-toolbar-title id="anchor" style="font-weight: bold;"
-        >Explore</v-toolbar-title
-      >
+      <v-toolbar-title style="font-weight: bold;">Explore</v-toolbar-title>
     </v-app-bar>
 
     <ais-instant-search index-name="classes" :search-client="searchClient">
