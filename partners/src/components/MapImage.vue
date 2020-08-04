61b9f53ca379ea0de9c4953f05549438bee21a6f
Jessica Jacelyn <jessicajacelyn@gmail.com>
2020-08-04 16:55:54 +0800

    Updated MapImage component with module from app.

--- partners/src/components/MapImage.vue
+++ partners/src/components/MapImage.vue
@@ -1,11 +1,9 @@
 <template>
-  <!-- <v-img :src="clas.locationImage" @click="openMaps" /> -->
   <v-img :src="locationImage" @click="openMaps" />
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import createGMapsImg from "../store/module/classes/gMapsImage";
+import createGMapsImg from "../utils/gMapsImage";
 
 /**
  * @todo Optimize usage of props. Perhaps can do away with src and use classID to get src if not provided.
@@ -14,18 +12,36 @@ import createGMapsImg from "../store/module/classes/gMapsImage";
 
 export default {
   name: "gMap",
-  props: ["src", "classID"],
+  props: ["src", "classID", "partnerID"],
   computed: {
-    locationImage() {
-      if (!this.src)
-        return createGMapsImg(
-          this.$store.state.classes.classes[this.classID].location.coordinates
-        );
-      else return this.src;
+    clas() {
+      return this.$store.state.classes.classes[this.classID];
+    },
+    partner() {
+      return this.$store.state.classes.partners[this.partnerID];
     },
     coordinates() {
-      return this.$store.state.classes.classes[this.classID].location
-        .coordinates;
+      // If image src is specified, ignore coordinates calculation
+      if (this.src) return;
+
+      // If the map image for a class is requested, check if there is a custom class location else use partner's location
+      // null coalescing to protect against undefined if clas or partner object is not loaded yet.
+      if (this.clas)
+        return (
+          this.clas?.location_coordinates ||
+          this.$store.state.classes.partners[this.clas.partnerID]
+            ?.location_coordinates
+        );
+      else if (this.partner) return this.partner?.location_coordinates;
+      else return undefined; // If neither clas nor partner is loaded yet, return nothing first
+    },
+    locationImage() {
+      // Only if src is not specified, create the image source link
+      if (!this.src) {
+        // Coordinates may not be calculated finish before this property is triggered
+        if (!this.coordinates) return;
+        else return createGMapsImg(this.coordinates);
+      } else return this.src;
     },
   },
   methods: {
