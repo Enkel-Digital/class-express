4dd87df64daf1beb4cd227d5131c12f4562619d3
Jessica Jacelyn <jessicajacelyn@gmail.com>
2020-08-05 18:41:52 +0800

    Created new partner module in vuex.

---
+++ partners/src/store/module/partner/index.js
@@ -0,0 +1,30 @@
+/**
+ * Vuex module for all things classes related including reviews
+ */
+
+import initialState from "./initialState";
+import setter from "../../utils/setter";
+import apiError from "@/store/utils/apiError";
+import apiWithLoader from "@/store/utils/apiWithLoader";
+// @todo Remove mock data
+import mock from "../../mockData";
+
+export default {
+  namespaced: true,
+  state: initialState(),
+  mutations: {
+    setter,
+  },
+  getters: {},
+  actions: {
+    async getPartnerDetails({ state, commit, dispatch }, partnerID) {
+      const response = await apiWithLoader.get(`/partner/details/${partnerID}`);
+      if (!response.success)
+        return apiError(response, () => dispatch("getPartnerDetails"));
+
+      console.log("partner", response);
+
+      commit("setter", ["partner", response.partner]);
+    },
+  },
+};
