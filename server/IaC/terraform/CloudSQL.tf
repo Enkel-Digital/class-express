32cf4e54e7f0644290d4faef5792977fdcd7e3e2
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-08-10 18:30:58 +0800

    [IaC] Setup CloudSQL with terraform

--- server/IaC/terraform/CloudSQL.tf
+++ server/IaC/terraform/CloudSQL.tf
@@ -1,31 +1,31 @@
-# Reference: https://www.terraform.io/docs/providers/google/r/sql_database_instance.html
+// configure the named provider: google
+provider "google" {
+  version = "3.5.0"
 
-resource "google_sql_database_instance" "master" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+
+
+# resource "google_sql_database" "database" {
+#   name     = "my-database"
+#   instance = "main-sql-instance"
+# }
+
+
+// reference: https://www.terraform.io/docs/providers/google/d/sql_database_instance.html
+resource "google_sql_database_instance" "postgres" {
   name             = "main-sql-instance"
   database_version = "POSTGRES_12"
   region           = "asia-southeast1" # Singapore
-  
-  # @todo To support replica/HA configs
-  # @todo Specify starting disk size
-  
-  settings {
-    # Second-generation instance tiers are based on the machine type. See argument reference below.
-    # @todo Change machine type, make sure it is supported by postgres
+
+   settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
     tier = "db-f1-micro"
-
-    # @todo Change to allow 0.0.0.0/0
-    # @todo Enforce secure connections only
-    ip_configuration {
-
-      # @todo To change this
-      dynamic "authorized_networks" {
-        for_each = google_compute_instance.apps
-        iterator = apps
-
-        content {
-          name  = apps.value.name
-          value = apps.value.network_interface.0.access_config.0.nat_ip
-        }
-      }
   }
 }
