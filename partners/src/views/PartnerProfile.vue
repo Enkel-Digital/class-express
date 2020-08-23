0214c89725af276dd2048cba2e9dca633055c1e6
Jaime <jaimeloeuf@gmail.com>
2020-08-23 23:53:56 +0800

    Fix PartnerProfile view UI.

--- partners/src/views/PartnerProfile.vue
+++ partners/src/views/PartnerProfile.vue
@@ -1,177 +1,175 @@
 <template>
   <v-main id="PartnerProfile">
     <v-row>
-      <v-col cols="8" sm="2">
-        <v-card outlined ref="form">
-          <br />
+      <v-card
+        outlined
+        class="mx-auto bottom-margin"
+        max-width="20em"
+        max-height="26em"
+      >
+        <!-- @todo Load the partner pic instead -->
+        <!-- @todo Allow partner admin to modify the partner images -->
+        <v-img
+          src="http://mimgnews1.naver.net/image/433/2018/06/21/0000046075_001_20180621082632801.jpg"
+          alt="Partner Main Image"
+          max-width="20em"
+          max-height="20em"
+        />
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>{{ partner.name }}</v-list-item-title>
+            <v-list-item-subtitle>
+              Number of employees:
+              {{ numberOfEmployees }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle>
+              Number of Classes:
+              {{ numberOfClass.length }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+
+      <v-card outlined class="mx-auto bottom-margin" width="40em">
+        <v-card-text>
+          <h2 style="color: #455a64;" class="text-left font-weight-light">
+            DETAILS
+          </h2>
           <br />
 
-          <!-- @todo Load the partner pic instead -->
-          <!-- @todo Allow partner admin to modify the partner images -->
-          <img
-            src="http://mimgnews1.naver.net/image/433/2018/06/21/0000046075_001_20180621082632801.jpg"
-            alt="Partner Main Image"
+          <v-text-field
+            v-model="partner.name"
+            :rules="nameRules"
+            label="Company Name"
+            color="#60696c"
+            required
           />
 
+          <v-text-field
+            v-model="partner.email"
+            :rules="nameRules"
+            label="Email"
+            placeholder="admin@example.com"
+            color="#60696c"
+            required
+          />
+
+          <v-text-field
+            v-model="partner.phoneNumber"
+            :rules="nameRules"
+            label="Telephone No."
+            placeholder="+65 91234567"
+            color="#60696c"
+            required
+          />
+
+          <v-text-field
+            v-model="partner.website"
+            label="Website"
+            placeholder="MUST BE HTTPS LIKE -->  https://example.com/"
+            color="#60696c"
+            required
+          />
+
+          <!-- @todo Integrate with API to show suggestions -->
+          <v-combobox
+            v-model="partnerTags"
+            :items="partnerTagsList"
+            chips
+            color="#60696c"
+            clearable
+            label="Company Categories"
+            multiple
+            single-line
+          >
+            <template v-slot:selection="{ attrs, item, select, selected }">
+              <v-chip
+                v-bind="attrs"
+                :input-value="selected"
+                close
+                small
+                @click="select"
+                @click:close="remove(item)"
+              >
+                <strong>{{ item }}</strong>
+              </v-chip>
+            </template>
+          </v-combobox>
+
+          <!-- @todo Add a tooltip to show the hints -->
+          <v-textarea
+            v-model="partner.description"
+            v-autofocus
+            type="text"
+            rows="5"
+            outlined
+            label="Company Description"
+            hint="You can use HTML for text formatting!"
+            no-resize
+            color="#60696c"
+            required
+          />
+        </v-card-text>
+      </v-card>
+
+      <br />
+
+      <v-card outlined ref="form" class="mx-auto bottom-margin" width="30em">
+        <v-card-text>
+          <h2 style="color: #455a64;" class="text-left font-weight-light">
+            ADDRESS
+          </h2>
           <br />
-          <br />
 
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>{{ partner.name }}</v-list-item-title>
-              <v-list-item-subtitle>
-                Number of employees:
-                {{ numberOfEmployees }}
-              </v-list-item-subtitle>
-              <v-list-item-subtitle>
-                Number of Classes:
-                {{ numberOfClass.length }}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-      </v-col>
+          <v-text-field
+            v-model="partner.location_address"
+            :rules="addressRules"
+            label="Address Line 1"
+            placeholder="123 Peach St"
+            color="#60696c"
+            required
+          />
 
-      <v-col cols="12" sm="5">
-        <v-card outlined ref="form">
-          <v-card-text>
-            <h2 style="color: #455a64;" class="text-left font-weight-light">
-              COMPANY DETAILS
-            </h2>
-            <br />
+          <v-text-field
+            label="Address Line 2"
+            placeholder=""
+            color="#60696c"
+          ></v-text-field>
 
-            <v-text-field
-              v-model="partner.name"
-              :rules="nameRules"
-              label="Company Name"
-              color="#60696c"
-              required
-            />
+          <v-text-field
+            label="Unit No."
+            color="#60696c"
+            placeholder="12-02"
+            required
+          ></v-text-field>
 
-            <v-text-field
-              v-model="partner.email"
-              :rules="nameRules"
-              label="Email"
-              placeholder="abc@enkel.com"
-              color="#60696c"
-              required
-            />
+          <v-text-field
+            :rules="nameRules"
+            label="Postal Code"
+            required
+            placeholder="111222"
+            color="#60696c"
+          ></v-text-field>
 
-            <v-text-field
-              v-model="partner.phoneNumber"
-              :rules="nameRules"
-              label="Telephone No."
-              placeholder="96475394"
-              color="#60696c"
-              required
-            />
-
-            <v-text-field
-              v-model="partner.website"
-              label="Website"
-              placeholder="guitar.com"
-              color="#60696c"
-              required
-            />
-
-            <v-combobox
-              v-model="partnerTags"
-              :items="partnerTagsList"
-              chips
-              color="#60696c"
-              clearable
-              label="Company Categories"
-              multiple
-              single-line
-            >
-              <template v-slot:selection="{ attrs, item, select, selected }">
-                <v-chip
-                  v-bind="attrs"
-                  :input-value="selected"
-                  close
-                  small
-                  @click="select"
-                  @click:close="remove(item)"
-                >
-                  <strong>{{ item }}</strong>
-                </v-chip>
-              </template>
-            </v-combobox>
-
-            <v-textarea
-              v-model="partner.description"
-              v-autofocus
-              type="text"
-              rows="5"
-              outlined
-              label="Company Description"
-              hint="use HTML text formatting"
-              no-resize
-              color="#60696c"
-              required
-            />
-          </v-card-text>
-        </v-card>
-      </v-col>
-
-      <v-col cols="12" sm="4">
-        <v-card outlined ref="form">
-          <v-card-text>
-            <h2 style="color: #455a64;" class="text-left font-weight-light">
-              COMPANY ADDRESS
-            </h2>
-            <br />
-
-            <v-text-field
-              v-model="partner.location_address"
-              :rules="addressRules"
-              label="Address Line 1"
-              placeholder="123 Peach St"
-              color="#60696c"
-              required
-            />
-
-            <v-text-field
-              label="Address Line 2"
-              placeholder=""
-              color="#60696c"
-            ></v-text-field>
-
-            <v-text-field
-              label="Unit No."
-              color="#60696c"
-              placeholder="12-02"
-              required
-            ></v-text-field>
-
-            <v-text-field
-              :rules="nameRules"
-              label="Postal Code"
-              required
-              placeholder="111222"
-              color="#60696c"
-            ></v-text-field>
-
-            <v-text-field
-              :rules="nameRules"
-              label="Country"
-              required
-              color="#60696c"
-              placeholder="Singapore"
-            ></v-text-field>
-          </v-card-text>
-        </v-card>
-      </v-col>
+          <v-text-field
+            :rules="nameRules"
+            label="Country"
+            required
+            color="#60696c"
+            placeholder="Singapore"
+          ></v-text-field>
+        </v-card-text>
+      </v-card>
     </v-row>
 
     <br />
-    <v-row class="text-center">
-      <v-col cols="12" sm="12">
-        <v-btn color="#60696c" rounded width="15em" outlined depressed large>
-          Save Changes
-        </v-btn>
-      </v-col>
-    </v-row>
+    <!-- @todo Update this to support color change depending on whether there is data changed or not -->
+    <v-btn color="#60696c" rounded width="15em" outlined depressed large>
+      Save Changes
+    </v-btn>
+
+    <!-- @todo Add a reset button to clear unsaved changes -->
   </v-main>
 </template>
 
@@ -227,5 +225,9 @@ export default {
 #PartnerProfile {
   margin: 1em;
   margin-top: 1em;
+}
+
+.bottom-margin {
+  margin-bottom: 2em;
 }
 </style>
