6cd251b2406ad6cc4870908e3ef95c173cfcea41
Jaime <jaimeloeuf@gmail.com>
2020-08-29 22:47:39 +0800

    [partner] Rewrote new partner API for new flow:     New flow to only allow partner creation after approval from the CE     team, instead of directly inserting into the DB.

--- server/partners/src/routes/partner.js
+++ server/partners/src/routes/partner.js
@@ -9,6 +9,7 @@ const express = require("express");
 const router = express.Router();
 const SQLdb = require("@enkeldigital/ce-sql");
 const getPartnerTags = require("../db/getPartnerTags");
+const sendMail = require("../utils/sendMail");
 
 const createLogger = require("@lionellbriones/logging").default;
 const logger = createLogger("routes:partner");
@@ -86,36 +87,87 @@ router.delete("/:partnerID", express.json(), async (req, res) => {
 
 /**
  * Create new partner / business organization
+ * @todo DOES NOT have auth token requirement, but should be rate limited (with IP / through unique email and phone verification)
  * @name POST /partner/new/
  * @param {Object} partner
- * @returns {object} success indicator and partnerID
- *
- * @todo Perhaps return a unique token for the admin that created this account to create an account without requiring admin approval...
- * @todo Should support like a hook system.
- * All the things that should be ran when a new user is created should be posted here as a hook
- * then on user creation, either call all the hooks, or publish a event for all the listeners to use.
- * Should only run webhook after validation? So if the first admin is not created yet, this should not be pushed to algolia
- *    if not ppl can just randomly create new business to pollute without any verification
- *    Should partner have a field that says, "users" or something to proof that there are owners of this partner?
+ * @param {Object} accountCreationRequest
+ * @returns {object} success indicator
  */
 router.post("/new", express.json(), async (req, res) => {
   try {
-    const { partner } = req.body;
+    const { accountCreationRequest, partner } = req.body;
+
+    // @todo Better verification
+    if (!partner) throw new Error("Missing 'partner' data");
+    if (!accountCreationRequest)
+      throw new Error("Missing 'owner account' data");
 
     const validateURL = require("../validations/isHTTPS");
 
     if (!validateURL(partner.website))
       throw new Error("Website URL should be HTTPS only");
 
-    // Insert the partner object and request for id of newly inserted row to be returned
-    // @todo Can I use .first here on the array
-    const partnerID = (
-      await SQLdb("partners").insert(partner).returning("id")
-    )[0];
+    // Insert partner details alongside data from accountCreationRequest into temporary DB, for CE admins to verify
+    await SQLdb("new_partners").insert({
+      ...partner,
+      createdByName: accountCreationRequest.name,
+      createdByEmail: accountCreationRequest.email,
+    });
 
-    // Return partnerID for frontend to use and create partner accounts
-    // @todo is ANYONE allowed to specify a partner ID and sign in from? Shouldnt we have a admin approval process
-    res.status(201).json({ success: true, partnerID });
+    // Create a HTML list for the details to send to CE admins for us to verify
+    const htmlDetails =
+      "<ul>" +
+      Object.entries(partner)
+        .map(([key, value]) => `<li>${key}: ${value}</li>`)
+        .join("") +
+      "</ul>";
+
+    // Notify the CE admins
+    // @todo Replace with teletif
+    await sendMail({
+      // @todo Replace domain once fixed
+      to: "classexpress@enkeldigital.com",
+      from: "Accounts@classexpress.com",
+      subject: `New ClassExpress partner ${partner.name}`,
+      // @todo Use a sendgrid template instead
+      html:
+        `A new ClassExpress partner has been registered and is now waiting for verification.<br />` +
+        "Here are the details.<br />" +
+        "<br />" +
+        htmlDetails,
+      // @todo Perhaps add link to allow us to verify directly or smth?
+      // @todo This part should be replaced by an Admin panel
+    });
+
+    // Send email to the company email address to let them know that verification is in progress
+    await sendMail({
+      to: partner.companyEmail,
+      from: "Accounts@classexpress.com",
+      subject: `ClassExpress partner registration for ${partner.name}`,
+      // @todo Use a sendgrid template instead
+      html:
+        `A new ClassExpress partner has been registered and is now waiting for verification.<br />` +
+        "Here are the details.<br />" +
+        "<br />" +
+        htmlDetails,
+    });
+
+    // Notify the business owner that their business is awaiting verification, and they will get another email for signup once verified
+    await sendMail({
+      to: accountCreationRequest.email,
+      from: "Accounts@classexpress.com",
+      subject: `Hi ${accountCreationRequest.name}, we received ${partner.name}'s ClassExpress partner registration`,
+      // @todo Use a sendgrid template instead
+      html:
+        `This email is to inform you that the registration and verification process for your ClassExpress partner has began.<br />` +
+        "Sit tight for now! We will be in contact with you shortly and inform you if there is any things else we need from you.<br />" +
+        "Once your business is verified, we will send this email a link to create an admin account for the business.<br />" +
+        "<br />" +
+        // @todo Replace the domain once it has been set
+        "Thank you so much for choosing us! If you need any further assistance, please contact us <a href='mailto:admin@enkeldigital.com'>here</a>.<br />",
+    });
+
+    res.status(201).json({ success: true });
   } catch (error) {
     logger.error(error);
     res.status(500).json({ success: false, error: error.message });
