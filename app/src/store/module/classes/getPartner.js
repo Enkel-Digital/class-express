8a2e506aede5152adc97f4b17013c9f9c25f1aba
Jaime <jaimeloeuf@gmail.com>
2020-09-17 23:19:04 +0800

    Fix get class/partner service for Queued values:     Fixes the issue of returning the API request promise instead of the     class or partner details object when calling get class/partner with     an ID that has already been requested for and is currently being     awaited/queued/running in the background.

--- app/src/store/module/classes/getPartner.js
+++ app/src/store/module/classes/getPartner.js
@@ -27,7 +27,31 @@ async function _getPartner(partners, commit, partnerID) {
   // Skip if partnerID is already requested for but not fulfilled yet
   // Return partner if in state and the promise (that is saved in the partnersToFetch queue) if it is still pending
   if (partners[partnerID]) return partners[partnerID];
-  if (partnersToFetch[partnerID]) return partnersToFetch[partnerID];
+
+  /*
+    When this function is called and the API request has been made, instead of returning the
+    promise in the queue directly, we await the promise here first for the caller to "await".
+    
+    This is because, the promise stored inside the partnersToFetch queue is actually the network request promise,
+    which resolves into the response object from the API. Since we only want to return either the Partner object
+    or undefined from this function, we need to await the request promise, before returning the Partner Object if
+    the API request succeeded, or do an explicit "return undefined" to match the function signature.
+    
+    Why doesnt this section need to do things like handle API error and state mutation?
+    tl;dr The code below handles it all when the API call promise resolves.
+    
+    Unlike the code below that awaits for the promise and handle deleting the promise from the Queue and
+    handling API errors, when the promise resolves, this section for awaiting the Partner object does not.
+    Since we can await the same promise multiple times, this is just another part that awaits for the same
+    promise. When the original call to the function / returned-promise, completes the API call, it will
+    handle all the things needed to be done, so this section just needs to await and return the Partner object
+    if succeeded and does not need to handle anything else.
+  */
+  if (partnersToFetch[partnerID]) {
+    const response = await partnersToFetch[partnerID];
+    if (response.success) return response.partner;
+    else return; // Explicit end of function to prevent execution from continuing and re-running the API call.
+  }
 
   const responsePromise = await api.get(`/partner/details/${partnerID}`);
 
@@ -39,8 +63,8 @@ async function _getPartner(partners, commit, partnerID) {
   // Clear partnerID immediately after API resolves to prevent this from being uncleared if retries if API failed
   delete partnersToFetch[partnerID];
 
-  // @todo Returning apiError will fk up caller's code, which may depend on getting back the class object or undefined
-  // @todo If res fails but the reason is because invalid classID, maybe I should do smth else instead?
+  // @todo Returning apiError will fk up caller's code, which may depend on getting back the Partner object or undefined
+  // @todo If res fails but the reason is because invalid partnerID, maybe I should do smth else instead?
   if (!response.success)
     return apiError(
       response,
