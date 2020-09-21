c9e2583b39e9ef956f71b4628b4b85d41ffcf70c
Jaime <jaimeloeuf@gmail.com>
2020-09-22 03:06:21 +0800

    Update Download section video switch logic.

--- landing/src/components/Download.vue
+++ landing/src/components/Download.vue
@@ -3,16 +3,15 @@
     <!-- Empty span tag here and below to force the actual content to be in the middle of the hero section vertically -->
     <span />
 
-    <!-- On mobile devices, hide the video and video name, then on click, open the youtube link in new tab or youtube app -->
-
     <div class="columns">
       <!-- Embed a youtube video here to show how to install the App on IOS or android or how to use it -->
       <!-- Only shown on desktop, and hidden for mobile/tablet sizes. -->
+      <!-- On mobile/tablets, clicking it opens the youtube link in new tab or youtube app -->
       <div id="youtubeVideo" class="column">
         <p class="title">{{ videoName }}</p>
 
         <iframe
-          :src="videoIFrameSrc"
+          :src="videoIFrameSrc + preferencesString"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
@@ -36,7 +35,10 @@
 
             <!-- Here are big tiles as buttons, that I can click to show how to download / use on IOS and or Android -->
             <div class="columns is-multiline">
-              <div class="column card features-card is-5">
+              <div
+                class="column card features-card is-5"
+                @click="switchVideo(videoConfigs.install_ios)"
+              >
                 <p class="title">IOS</p>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -45,7 +47,10 @@
                   facilisis.
                 </p>
               </div>
-              <div class="column card features-card is-5">
+              <div
+                class="column card features-card is-5"
+                @click="switchVideo(videoConfigs.install_android)"
+              >
                 <p class="title">Android</p>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -67,7 +72,10 @@
 
             <!-- Here are big tiles as buttons, that I can click to show how to download / use on IOS and or Android -->
             <div class="columns is-multiline">
-              <div class="column card features-card is-5">
+              <div
+                class="column card features-card is-5"
+                @click="switchVideo(videoConfigs.gettingStarted)"
+              >
                 <p class="title">Getting Started</p>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -76,7 +84,10 @@
                   facilisis.
                 </p>
               </div>
-              <div class="column card features-card is-5">
+              <div
+                class="column card features-card is-5"
+                @click="switchVideo(videoConfigs.findClass)"
+              >
                 <p class="title">Find a new Class</p>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -99,12 +110,24 @@
 <script>
 export default {
   name: "Download",
+
+  created() {
+    // @todo Replace this makeshift solution with smth that Detect what device, and show the ios or android loading one
+    if (!this.ifMobileOrTablet())
+      this.switchVideo(this.videoConfigs.install_ios);
+  },
+
   data() {
+    //   @todo Explore the enableJsonAPI URL query parameter
+
     const preferences = {
       cc_lang_pref: "en",
       cc_load_policy: "1",
-      autoplay: "1",
-      loop: "1",
+
+      // autoplay: "1", // To enable this, need to either set playlist value or set mute to true
+
+      // loop: "1",
+      // playlist: "86D7AGm5sHk", // To enable autoplay, the playlist must be set to the video ID
     };
 
     const preferencesString =
@@ -116,11 +139,52 @@ export default {
     console.log(preferencesString);
 
     return {
-      videoName: "Name of the video",
+      videoName: "",
+      videoIFrameSrc: "",
       preferencesString,
-      videoIFrameSrc: "https://www.youtube-nocookie.com/embed/86D7AGm5sHk",
-      // "https://www.youtube-nocookie.com/embed/egHjkeiqjrA"
+
+      videoConfigs: {
+        install_ios: {
+          videoIFrameSrc: "https://www.youtube-nocookie.com/embed/86D7AGm5sHk",
+          videoName: "Install for IOS", // ios 13 and above for the new UI
+        },
+        install_android: {
+          videoIFrameSrc: "https://www.youtube-nocookie.com/embed/egHjkeiqjrA",
+          videoName: "Install for Android",
+        },
+        gettingStarted: {
+          videoIFrameSrc: "https://www.youtube-nocookie.com/embed/w0P0FQ770dE",
+          videoName: "Getting Started",
+        },
+        findClass: {
+          videoIFrameSrc: "https://www.youtube-nocookie.com/embed/hs1HoLs4SD0",
+          videoName: "Find a New Class",
+        },
+      },
     };
+  },
+
+  methods: {
+    ifMobileOrTablet() {
+      return (
+        Math.max(
+          document.documentElement.clientWidth || 0,
+          window.innerWidth || 0
+        ) < 1024
+      );
+    },
+
+    switchVideo(videoConfig) {
+      // Just like a media query, where this is true if using tablet or mobile device
+      if (this.ifMobileOrTablet())
+        return window.open(
+          `https://youtube.com/watch?v=${videoConfig.videoIFrameSrc.slice(39)}`,
+          "_blank"
+        );
+
+      this.videoIFrameSrc = videoConfig.videoIFrameSrc;
+      this.videoName = videoConfig.videoName;
+    },
   },
 };
 </script>
@@ -130,6 +194,8 @@ export default {
   border-radius: 1em;
   border: 1px solid rgb(220, 220, 220);
   margin: 1em;
+
+  cursor: pointer;
 }
 
 /* Change background depending on screen size */
