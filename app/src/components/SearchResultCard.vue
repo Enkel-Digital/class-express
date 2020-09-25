127f1b362b7d32d7e8571c826d68134d25d8321e
Jaime <jaimeloeuf@gmail.com>
2020-09-26 02:02:52 +0800

    Fix click to details bug in SearchResultCard: 1.  Update the area in SearchResultCard that is clickable to redirect to     the class or partner details page. 2.  Fix the toggle favourite button, implementing 2 seperate buttons     depending on the search result type, since this component handles     both results for class and partners.

--- app/src/components/SearchResultCard.vue
+++ app/src/components/SearchResultCard.vue
@@ -2,20 +2,15 @@
   <!-- @todo Handle cases where sometimes classID sometimes partnerID -->
   <v-card
     v-if="item"
-    @click="
-      $router.push({
-        name: 'ClassDetails',
-        params: { classID: item.classID },
-      })
-    "
     class="mx-auto"
     width="calc(100% - 1.2em)"
     style="margin-bottom: 0.5em"
     outlined
     :ripple="false"
   >
-    <v-row class="">
-      <v-col class="pa-0 pl-3">
+    <v-row>
+      <!-- Clicking on the image brings you to the details page -->
+      <v-col class="pa-0 pl-3" @click="gotoDetails(item.classID)">
         <!-- Height set to 100% to scale the image to cover the entire column, regardless of how long the other column is! -->
         <v-img
           height="100%"
@@ -26,31 +21,47 @@
       </v-col>
 
       <v-col class="pr-6" style="text-align: left">
-        <ais-highlight :hit="item" attribute="name" />
-        <hr />
+        <!-- Clicking on the section of words excluding the buttons section brings you to the details page -->
+        <div @click="gotoDetails(item.classID)">
+          <ais-highlight :hit="item" attribute="name" />
+          <hr />
 
-        <!-- @todo For the description might want to crop it here or the backend to limit the character length to
+          <!-- @todo For the description might want to crop it here or the backend to limit the character length to
             prevent this from being too long and blocking everyone else out and over stretching the image-->
-        <ais-highlight
-          :hit="item"
-          attribute="description"
-          style="font-size: 0.9em"
-        />
-        <!-- <p><ais-highlight :hit="item" attribute="description" /></p> -->
-        <hr />
+          <ais-highlight
+            :hit="item"
+            attribute="description"
+            style="font-size: 0.9em"
+          />
+          <!-- <p><ais-highlight :hit="item" attribute="description" /></p> -->
+          <hr />
 
-        <!-- {{ item.provider?.name }} -->
-        <!-- <hr /> -->
+          <!-- @todo Maybe show the _tags? -->
+          <!-- {{ item.provider?.name }} -->
+          <!-- <hr /> -->
 
-        <p style="font-size: 0.8em">At: {{ item.location_address }}</p>
+          <p style="font-size: 0.8em">At: {{ item.location_address }}</p>
+        </div>
 
         <div class="float-right pb-0">
-          <!-- @todo what about partnerIDs? -->
-          <!-- Must remember to load fav classes first -->
-          <v-btn icon @click="toggleFavouriteClass(clas.id)">
+          <!-- @todo Must remember to load fav classes first -->
+          <v-btn
+            icon
+            v-if="item.classID"
+            @click="toggleFavouriteClass(item.classID)"
+          >
             <v-icon v-if="isFavourite" color="red">mdi-heart</v-icon>
             <v-icon v-else>mdi-heart-outline</v-icon>
           </v-btn>
+          <v-btn
+            icon
+            v-else-if="item.partnerID"
+            @click="toggleFavouritePartner(item.partnerID)"
+          >
+            <v-icon v-if="isFavourite" color="red">mdi-heart</v-icon>
+            <v-icon v-else>mdi-heart-outline</v-icon>
+          </v-btn>
+          <!-- No v-else here, as it can be assumed that the search item will always either have a classID or partnerID -->
 
           <!-- @todo Extract out all share buttons to a common component -->
           <!-- @todo Implement PWA sharing and web share target code  -->
@@ -64,6 +75,7 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import { AisHighlight } from "vue-instantsearch";
 
 export default {
@@ -78,6 +90,27 @@ export default {
         // Using parseInt on classID as it is a URL param prop passed in as a string via vue router
         (classID) => classID === parseInt(this.item.classID)
       );
+    },
+  },
+  methods: {
+    ...mapActions("classes", [
+      "toggleFavouriteClass",
+      "toggleFavouritePartner",
+    ]),
+    // Accepts an ID that can either be a classID or partnerID, depending on whether this search result is a class or partner
+    gotoDetails(id) {
+      // Checking for classID first, as every class have a partnerID but no partner have a classID
+      if (this.item.classID)
+        this.$router.push({
+          name: "ClassDetails",
+          params: { classID: id },
+        });
+      else if (this.item.partnerID)
+        this.$router.push({
+          name: "partner",
+          params: { partnerID: id },
+        });
+      else throw new Error("Search Item missing both classID and partnerID");
     },
   },
 };
