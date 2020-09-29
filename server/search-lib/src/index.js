0fc4e076e239ac9caebd91d7446ecdfef8ea99b8
Jaime <jaimeloeuf@gmail.com>
2020-09-29 15:55:02 +0800

    [search-lib] Re-implement lib with proxy object: 1.  Replace algoliaIndex with algoliaClient, requiring user to explicitly     pass in the index name to initialize. 2.  Remove add/update/delete method modules, to be replaced with direct     implementation in the proxy object. 3.  Wrote new proxy object exposed as library default entrance, to update     the API to allow the prop called on the search object to be used as     the name of the search index to make it more ergonomic, and uses proxy     object to allow chaining on the index name to call get/update/delete     methods for that specific index.

--- server/search-lib/src/index.js
+++ server/search-lib/src/index.js
@@ -1,11 +1,79 @@
+/**
+ * Module to work with algolia search indices in a more generalized and ergonomic API
+ * @author JJ
+ */
+
 // Setup environment variables
 require("dotenv").config();
+const executeSearchOperation = require("./executeSearchOperation");
 
-module.exports = {
-  algoliaIndex: require("./algoliaIndex"),
-  executeSearchOperation: require("./executeSearchOperation"),
+// The actual search object, holding all the methods for the lib user
+const search = {
+  algoliaClient: require("./algoliaClient"),
   transformID: require("./transformID"),
-  ...require("./add"),
-  ...require("./update"),
-  ...require("./delete"),
+  executeSearchOperation,
+
+  useIndex(index) {
+    return new Proxy(
+      {},
+      {
+        // @todo See how to not use target and not get an error here
+        get(target, prop) {
+          //   toLowerCase the prop
+
+          const operation = {
+            add: async (searchObjects, prependedString) =>
+              executeSearchOperation(
+                index.saveObject,
+                searchObjects,
+                prependedString
+              ),
+
+            update: async (searchObjects, prependedString) =>
+              executeSearchOperation(
+                (searchObject) =>
+                  index.partialUpdateObject(
+                    searchObject,
+                    { createIfNotExists: false } // Do not allow API caller to use this API to create new search objects.
+                  ),
+                searchObjects,
+                prependedString
+              ),
+
+            del: async (id, prependedString) =>
+              index.deleteObject(`${prependedString}${id}`),
+          }[prop];
+
+          if (!operation)
+            throw new Error(
+              "Only search index modification methods can be chained on useIndex"
+            );
+
+          return operation;
+        },
+      }
+    );
+  },
 };
+
+// Export a proxy around the search object
+module.exports = new Proxy(search, {
+  get(target, prop) {
+    switch (prop) {
+      case "algoliaClient":
+      case "executeSearchOperation":
+      case "transformID":
+        // @todo See whats the diff
+        // return Reflect.get(...arguments);
+        return target[prop];
+
+      // The prop will be treated as the index name
+      default:
+        return search.useIndex(target.algoliaClient.initIndex(prop));
+    }
+  },
+
+  set() {
+    throw new Error("CANNOT SET VALUES ON SEARCH OBJECT");
+  },
+});
