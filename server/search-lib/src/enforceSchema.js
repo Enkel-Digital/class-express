389a9527651b6a1f0c49c63de4605c044b8c3999
Jaime <jaimeloeuf@gmail.com>
2020-10-07 14:38:34 +0800

    [search-lib] Add walkIn/maxParticipants to schema:     Allow these 2 additional fields through the enforceSchema function     as these 2 fields will now be useable as search attributes.

--- server/search-lib/src/enforceSchema.js
+++ server/search-lib/src/enforceSchema.js
@@ -1,5 +1,6 @@
 /**
  * Explicitly use the data values to prevent extra values from being accidentally inserted
+ * @todo Might split this up once we split up class and partner to use different indexes
  * @param originalObject
  */
 module.exports = function enforceSchema(originalObject) {
@@ -12,6 +13,8 @@ module.exports = function enforceSchema(originalObject) {
     location_address: originalObject.location_address,
     pictureSources: originalObject.pictureSources,
     points: originalObject.points,
+    maxParticipants: originalObject.maxParticipants,
+    walkIn: originalObject.walkIn,
     _tags: originalObject.tags,
   };
 };
