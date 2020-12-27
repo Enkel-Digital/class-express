28987b4ef82315d3e389b4b15d681f88e76595c1
Jaime <jaimeloeuf@gmail.com>
2020-12-27 11:07:49 +0800

    [DB] Refactor common config in knexfile.

--- server/DB/ce-sql/src/knexfile.js
+++ server/DB/ce-sql/src/knexfile.js
@@ -1,78 +1,77 @@
-// Configurations for the different Databases
-// All env variables are pre-fixed with "SQL_"
+/**
+ * @module knexfile
+ * Configurations for the different Databases.
+ */
+
+const client = "postgresql";
+
+/**
+ * Default connection value
+ *
+ * Uses encodeURIComponent
+ *
+ * All env variables are pre-fixed with "SQL_"
+ */
+const connection = {
+  host: encodeURIComponent(process.env.SQL_HOSTNAME) || "localhost",
+  database: encodeURIComponent(process.env.SQL_DB_NAME) || null, // Null makes knex connect to the default DB, which is usually "postgres"
+  port: encodeURIComponent(process.env.SQL_PORT) || 5432,
+  user: encodeURIComponent(process.env.SQL_USERNAME) || "postgres",
+  password: encodeURIComponent(process.env.SQL_PASSWORD) || "password",
+};
+
+/**
+ * Default pool value
+ */
+const pool = {
+  min: 0,
+  max: 10,
+  createTimeoutMillis: 3000,
+  acquireTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+  reapIntervalMillis: 1000,
+  createRetryIntervalMillis: 100,
+  propagateCreateError: false,
+};
+
+/**
+ * Default migrations value
+ */
+const migrations = {
+  tableName: "knex_migrations",
+  directory: "./migrations",
+};
+
 module.exports = {
   development: {
-    client: "postgresql",
+    client,
     connection: {
-      host: "localhost",
+      ...connection,
+
+      // Overrides the default database name
       database: "CE",
-      port: 5432,
-      user: "postgres",
-      password: "password",
     },
-    pool: {
-      min: 0,
-      max: 10,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
+    pool,
+    migrations,
   },
 
   staging: {
-    client: "postgresql",
-    connection: {
-      host: encodeURIComponent(process.env.SQL_HOSTNAME),
-      database: encodeURIComponent(process.env.SQL_DB_NAME),
-      port: encodeURIComponent(process.env.SQL_PORT),
-      user: encodeURIComponent(process.env.SQL_USERNAME),
-      password: encodeURIComponent(process.env.SQL_PASSWORD),
-    },
-    pool: {
-      min: 0,
-      max: 10,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
+    client,
+    connection,
+    pool,
+    migrations,
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      host: encodeURIComponent(process.env.SQL_HOSTNAME),
-      database: encodeURIComponent(process.env.SQL_DB_NAME),
-      port: encodeURIComponent(process.env.SQL_PORT),
-      user: encodeURIComponent(process.env.SQL_USERNAME),
-      password: encodeURIComponent(process.env.SQL_PASSWORD),
-    },
+    client,
+    connection,
     pool: {
-      min: 0,
+      ...pool,
+
+      // Override default pool values
       max: 100,
-      createTimeoutMillis: 3000,
       acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false,
     },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
+    migrations,
   },
 };
